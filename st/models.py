a1af3a6c40771825642a75baa73e67155ee89fde
erenturkay <erenturkay@868bde64-912e-0410-afa2-bbf675bde9fd>
2007-06-09 18:14:00 +0000

    böylesi daha güzel gibi
    PS: bu fonksiyon ne işe yarıyor? :S :)

--- st/models.py
+++ st/models.py
@@ -60,7 +60,7 @@ class ScreenShot(models.Model):
     def __str__(self):
         return self.file
 
-    def get_thumbnail_url(file, size='230x230'):
+    def get_thumbnail_url(file, size='230,230'):
         thumb = 'thumb_' + file
         thumb_filename = os.path.join(settings.MEDIA_ROOT, thumb)
         thumb_url = os.path.join(settings.MEDIA_URL, thumb)
@@ -69,7 +69,7 @@ class ScreenShot(models.Model):
             filename = os.path.join(settings.MEDIA_ROOT, file)
             image = Image.open(filename)
 
-            s = size.split("x")
+            s = size.split(",")
             image.thumbnail([s[0], s[1]])
 
             image.save(thumb_filename, image.format)
