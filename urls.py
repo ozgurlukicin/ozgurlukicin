f4c815aba8d6fff3bae49e69353f9ebde6bdad86
erenturkay <erenturkay@868bde64-912e-0410-afa2-bbf675bde9fd>
2007-06-14 10:54:38 +0000

    kayıt için hazırlık yapalım, form için django.newforms kullanıyorum, haberiniz olsun :)

--- urls.py
+++ urls.py
@@ -33,6 +33,7 @@ urlpatterns = patterns('',
     #User management
     (r'^user/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
     (r'^user/logout/$', 'django.contrib.auth.views.logout', {'template_name': 'logout.html'}),
+    (r'^user/register/$', 'oi.st.views.user_register'),
     (r'^user/dashboard/$', 'oi.st.views.user_dashboard'),
     (r'^user/page/(?P<page>[0-9]+)/$', 'django.views.generic.list_detail.object_list', dict(user_dict)),
     (r'^user/(?P<name>.*)/$', 'oi.st.views.user_profile'),
