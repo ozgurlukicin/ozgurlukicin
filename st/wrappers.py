3612d2003ec58f6b0b131fd5472d05872e5ec68a
erenturkay <erenturkay@868bde64-912e-0410-afa2-bbf675bde9fd>
2007-06-15 13:47:17 +0000

    değişken operatorune boşluk verelim, sadece takıntı :P

--- st/wrappers.py
+++ st/wrappers.py
@@ -9,5 +9,5 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def render_response(req, *args, **kwargs):
-    kwargs['context_instance']= RequestContext(req)
+    kwargs['context_instance'] = RequestContext(req)
     return render_to_response(*args, **kwargs)
