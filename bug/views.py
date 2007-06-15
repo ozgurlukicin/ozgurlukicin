e4df40097cb7b6c3aa8a29e7c085c2f73f8922fa
erenturkay <erenturkay@868bde64-912e-0410-afa2-bbf675bde9fd>
2007-06-15 13:46:35 +0000

    template içinde user nesnesine erişmek için render_response wrapper fonksiyonunu kullanmalıyız. render_to_response RequestContext yüklemiyor.

--- bug/views.py
+++ bug/views.py
@@ -5,13 +5,13 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
-from django.shortcuts import render_to_response
+from oi.st.wrappers import render_response
 from oi.bug.models import Bug
 
 def main(request):
     bugs = Bug.objects.all()
-    return render_to_response('bug/bug_main.html', locals())
+    return render_response(request, 'bug/bug_main.html', locals())
 
 def detail(request, id):
     bug = Bug.objects.get(id=id)
-    return render_to_response('bug/bug_detail.html', locals())
+    return render_response(request, 'bug/bug_detail.html', locals())
