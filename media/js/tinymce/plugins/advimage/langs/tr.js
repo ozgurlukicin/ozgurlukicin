b59403f6c4faecef95707812ab82975bc2b38c10
havan <havan@868bde64-912e-0410-afa2-bbf675bde9fd>
2007-06-19 20:43:35 +0000

    tinMCE Türkçe konuşmaya başlasın.. Çevirileri hızla yapıyorum yanlışlarım olabilir

---
+++ media/js/tinymce/plugins/advimage/langs/tr.js
@@ -0,0 +1,27 @@
+// TR lang variables
+
+tinyMCE.addToLang('advimage',{
+tab_general : 'Genel',
+tab_appearance : 'Görünüm',
+tab_advanced : 'Gelişmiş',
+general : 'Genel',
+title : 'Başlık',
+preview : 'Önizmele',
+constrain_proportions : 'Oranları koru',
+langdir : 'Yazı yönü',
+langcode : 'Dil kodu',
+long_desc : 'Uzun açıklama linki',
+style : 'Stil',
+classes : 'Sınıflar',
+ltr : 'Soldan sağa',
+rtl : 'Sağdan sola',
+id : 'Id',
+image_map : 'Resim haritası',
+swap_image : 'Resmi değiştir',
+alt_image : 'Alternatif resim',
+mouseover : 'Fare üzerine geldiğinde',
+mouseout : 'Fare üzerinden çıktığında',
+misc : 'Muhtelif Özellikler',
+example_img : 'Görünüm&nbsp;önizleme&nbsp;resim',
+missing_alt : 'Resim açıklaması eklemeden devam etmek istediğinize emin misiniz? Devam ederseniz yazı tabanlı tarayıcılar kullananlar ile resimler kapalı halde tarayıcı kullananlar bu resimlere ulaşamazlar.'
+});
