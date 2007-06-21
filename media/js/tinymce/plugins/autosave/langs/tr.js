021f33dc851d9b2cc849cda8984003471248d91d
havan <havan@868bde64-912e-0410-afa2-bbf675bde9fd>
2007-06-21 14:33:37 +0000

    Ufak tercüme değişiklikleri

--- media/js/tinymce/plugins/autosave/langs/tr.js
+++ media/js/tinymce/plugins/autosave/langs/tr.js
@@ -1,5 +1,5 @@
 // TR lang variables
 
 tinyMCE.addToLang('',{
-autosave_unload_msg : 'Bu sayfadan çıkarsanız yaptığınız değişiklikler kaybedilecek.'
+autosave_unload_msg : 'Bu sayfadan çıkarsanız yaptığınız değişiklikler silinecek.'
 });
