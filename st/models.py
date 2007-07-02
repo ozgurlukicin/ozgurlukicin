9e8a3c0e568a66990cf9b0f3d5a32bfc81dc3251
ahmet <ahmet@868bde64-912e-0410-afa2-bbf675bde9fd>
2007-07-02 15:24:56 +0000

    profili ayır

--- st/models.py
+++ st/models.py
@@ -11,14 +11,9 @@ from commands import getoutput
 
 from django.db import models
 from django.contrib.auth.models import User
-from django import newforms as forms
 
 from oi.middleware import threadlocals
 from oi.settings import CITY_LIST, MEDIA_ROOT, MEDIA_URL
-
-#==========
-# DB Models
-#==========
 
 class Tag(models.Model):
     name = models.CharField('Etiket', maxlength = 32, blank=False, unique=True)
@@ -39,10 +34,8 @@ class Tag(models.Model):
         verbose_name = "Etiket"
         verbose_name_plural = "Etiketler"
 
-
 class Contribute(models.Model):
-    """ Contributes select field in user register page """
-    name = models.CharField('Ad', maxlength=32, blank=False, unique=True)
+    name = models.CharField('Tanım', maxlength=64, blank=False, unique=True)
 
     def __str__(self):
         return self.name
@@ -55,51 +48,6 @@ class Contribute(models.Model):
     class Meta:
         verbose_name = "Katkı Adı"
         verbose_name_plural = "Katkı Adları"
-
-class IgnoredUsername(models.Model):
-    """ Ignored user name model to prevent the site from slang :) This is controlled in register page"""
-    name = models.CharField("Kullanıcı adı", maxlength=30)
-
-    def __str__(self):
-        return self.name
-
-    class Admin:
-        list_display = ('name',)
-        ordering = ['-name']
-        search_fields = ['name']
-
-    class Meta:
-        verbose_name = "Yasaklanan Kullanıcı Adı"
-        verbose_name_plural = "Yasaklanan Kullanıcı Adları"
-
-class UserProfile(models.Model):
-    user = models.ForeignKey(User, unique=True)
-    homepage = models.URLField('Ana Sayfa', blank=True)
-    birthday = models.DateField()
-    im = models.EmailField('Bağlantı Adresi', blank=True, help_text='Jabber, Gtalk, Msn vs.')
-    city = models.CharField('Şehir', choices=CITY_LIST, maxlength=40)
-    show_email = models.BooleanField('E-posta Göster', default=0)
-    contributes = models.ManyToManyField(Contribute, blank=True, verbose_name='Katkılar')
-    contributes_summary = models.TextField('Katkı Açıklaması', blank=True)
-    activation_key = models.CharField(maxlength=40)
-    key_expires = models.DateTimeField()
-
-    def __str__(self):
-        return self.user.username
-
-    class Admin:
-        fields = (
-            ('Üyelik Bilgileri', {'fields': ('homepage','im', 'city', 'birthday', 'contributes', 'contributes_summary', 'show_email',)}),
-            ('Diğer', {'fields': ('activation_key', 'key_expires'), 'classes': 'collapse',}),
-        )
-
-        list_display = ('user', 'city',)
-        ordering = ['-user']
-        search_fields = ['user']
-
-    class Meta:
-        verbose_name = "Kullanıcı Profili"
-        verbose_name_plural = "Kullanıcı Profilleri"
 
 class ScreenShot(models.Model):
     desc = models.TextField('Açıklama')
@@ -407,132 +355,3 @@ class PardusMirror(models.Model):
     class Meta:
         verbose_name = "Pardus Yansısı"
         verbose_name_plural = "Pardus Yansıları"
-
-#======
-# Forms
-#======
-
-class RegisterForm(forms.Form):
-    username = forms.CharField(label='Kullanıcı Adı', max_length=30, help_text='En az 3, en fazla 30 karakter')
-    firstname = forms.CharField(label='İsim', max_length=30)
-    lastname = forms.CharField(label='Soyisim', max_length=30)
-    birthday = forms.DateField(label='Doğum Tarihi', help_text='10/5/1985 gibi.')
-    email = forms.EmailField(label='E-Posta')
-    password = forms.CharField(label='Parola', max_length=32, widget=forms.PasswordInput,)
-    password_again = forms.CharField(label='Parola (tekrar)', max_length=32, widget=forms.PasswordInput, help_text='En az 5 karakter')
-    city = forms.ChoiceField(label='Şehir', choices=CITY_LIST)
-    homepage = forms.URLField(label='Web Sayfası', verify_exists=False, required=False, help_text='(zorunlu değil)')
-    contributes = forms.ModelMultipleChoiceField(label='Katkı Başlıkları', queryset=Contribute.objects.all(), required=False, help_text='Bize nasıl katkıda bulunabilirsiniz? (ctrl ile birden fazla seçim yapılabilir, zorunlu değil)')
-    contributes_summary = forms.CharField(label='Açıklama', widget=forms.Textarea, required=False, help_text='Katkı sağlayabilecekseniz açıklama yazın (zorunlu değil)')
-    show_email = forms.BooleanField(label='E-posta Adresini Göster', required=False, help_text='Profil sayfasında diğerleri e-posta adresinizi görebilsin mi?')
-
-    def clean_username(self):
-        field_data = self.clean_data['username']
-
-        if not field_data:
-            return ''
-
-        if len(field_data.split(' ')) != 1:
-            raise forms.ValidationError(u"Kullanıcı adında boşluk olmamalıdır")
-
-        if len(field_data) < 3:
-            raise forms.ValidationError(u"Kullanıcı adı en az 3 karakter olmalıdır")
-
-        regexp = re.compile("\w+")
-        if not regexp.match(field_data):
-            raise forms.ValidationError(u"Kullanıcı adı geçersiz")
-
-        ignored = IgnoredUsername.objects.filter(name__iexact=field_data)
-        if len(ignored) > 0:
-            raise forms.ValidationError(u"Bu kullanıcı adının alınması yasaklanmış")
-
-        u = User.objects.filter(username__iexact=field_data)
-        if len(u) > 0:
-            raise forms.ValidationError(u"Bu kullanıcı adı daha önce alınmış")
-
-        return self.clean_data['username']
-
-
-    def clean_password_again(self):
-        field_data = self.clean_data['password_again']
-        password = self.clean_data['password']
-        if not field_data:
-            return ''
-
-        if len(field_data.split(' ')) != 1:
-            raise forms.ValidationError(u"Parolada boşluk olmamalıdır")
-
-        if len(field_data) < 5:
-            raise forms.ValidationError(u"Parola en az 5 karakter olmalıdır")
-
-        if (password or field_data) and password != field_data:
-                    raise forms.ValidationError(u"Parolalar eşleşmiyor")
-
-        return field_data
-
-class ProfileEditForm(forms.Form):
-    firstname = forms.CharField(label='İsim', max_length=30)
-    lastname = forms.CharField(label='Soyisim', max_length=30)
-    birthday = forms.DateField(label='Doğum Tarihi', help_text='10/5/1985 gibi.')
-    email = forms.EmailField(label='E-posta')
-    city = forms.ChoiceField(label='Şehir', choices=CITY_LIST)
-    homepage = forms.URLField(label='Ana Sayfa', required=False, help_text='http:// ile başlamayı unutmayın')
-    old_password = forms.CharField(label='Eski Parola', widget=forms.PasswordInput, max_length=32, required=False)
-    password = forms.CharField(label='Parola', widget=forms.PasswordInput, max_length=32, required=False, help_text='Değiştirmek istiyorsanız her ikisini de doldurun')
-    password_again = forms.CharField(label='Parola (yeniden)', widget=forms.PasswordInput, max_length=32, required=False)
-    show_email = forms.BooleanField(label='E-posta Adresini Göster', required=False, help_text='Profil sayfasında diğerleri e-posta adresinizi görsün mü?')
-
-    def set_user(self, user):
-        self.user = user
-
-    def clean_old_password(self):
-        field_data = self.clean_data['old_password']
-        if not field_data:
-            return ''
-        else:
-            if len(field_data.split(' ')) != 1:
-                raise forms.ValidationError(u"Parolada boşluk olmamalıdır")
-
-            if len(field_data) < 5:
-                raise forms.ValidationError(u"Parola en az 5 karakter olmalıdır")
-
-            return field_data
-
-    def clean_password(self):
-        field_data = self.clean_data['password']
-        if not field_data:
-            return ''
-        else:
-            if len(field_data.split(' ')) != 1:
-                raise forms.ValidationError(u"Parolada boşluk olmamalıdır")
-
-            if len(field_data) < 5:
-                raise forms.ValidationError(u"Parola en az 5 karakter olmalıdır")
-
-            return field_data
-
-    def clean_password_again(self):
-        field_data = self.clean_data['password_again']
-        password = self.clean_data['password']
-        old_password = self.clean_data['old_password']
-
-        if old_password or password or field_data:
-            if field_data and password and old_password:
-                if len(field_data.split(' ')) != 1:
-                    raise forms.ValidationError(u"Parolada boşluk olmamalıdır")
-
-                if len(field_data) < 5:
-                    raise forms.ValidationError(u"Parola en az 5 karakter olmalıdır")
-
-                if (password or field_data) and password != field_data:
-                    raise forms.ValidationError(u"Parolalar eşleşmiyor")
-
-                u = User.objects.get(username=self.user.username)
-                if not u.check_password(old_password):
-                    raise forms.ValidationError(u"Eski parola yanlış")
-
-                return field_data
-            else:
-                raise forms.ValidationError(u"Parolayı değiştirmek için her 3 alanı da doldurun")
-        else:
-            return ''
