59d83dd03f6acaba6a6a901f623b8a6a9ed3f191
erenturkay <erenturkay@868bde64-912e-0410-afa2-bbf675bde9fd>
2007-07-16 16:11:21 +0000

    değişken olsun bari

--- st/feeds.py
+++ st/feeds.py
@@ -8,6 +8,7 @@
 from django.contrib.syndication.feeds import Feed
 
 from oi.st.models import Game, FS, News, Package
+from oi.settings import NEWS_IN_HOMEPAGE, PACKAGES_IN_HOMEPAGE, GAMES_IN_HOMAPAGE, FS_IN_HOMEPAGE
 
 class RssMainFeed(Feed):
     title = 'Özgürlükİçin Pardus...'
@@ -16,19 +17,19 @@ class RssMainFeed(Feed):
 
     def items(self):
         output = []
-        for news in News.objects.order_by('-date')[:2]:
+        for news in News.objects.order_by('-date')[:NEWS_IN_HOMEPAGE]:
             news.title = 'Haber: %s' % news.title
             output.append(news)
 
-        for package in Package.objects.order_by('-update')[:2]:
+        for package in Package.objects.order_by('-update')[:PACKAGES_IN_HOMEPAGE]:
             package.name = 'Paket: %s' % package.name
             output.append(package)
 
-        for game in Game.objects.order_by('-update')[:2]:
+        for game in Game.objects.order_by('-update')[:GAMES_IN_HOMEPAGE]:
             game.title = 'Oyun: %s' % game.title
             output.append(game)
 
-        for fs in FS.objects.order_by('-update')[:2]:
+        for fs in FS.objects.order_by('-update')[:FS_IN_HOMEPAGE]:
             fs.title = 'İlk Adım: %s' % fs.title
             output.append(fs)
 
