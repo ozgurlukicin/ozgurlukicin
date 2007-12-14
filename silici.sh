28f2d7b6c13f837fc703d93de3bd279b05124e01
ahmet <ahmet@868bde64-912e-0410-afa2-bbf675bde9fd>
2007-12-14 12:22:22 +0000

    silelim

--- silici.sh
+++ silici.sh
@@ -1,14 +1,1 @@
-rm -f *.pyc
-rm -rf sanat/*.pyc
-rm -rf flatpages/*.pyc
-rm -rf st/*.pyc
-rm -rf sanat/*.pyc
-rm -rf profile/*.pyc
-rm -rf media/*.pyc
-rm -rf feedjack/*.pyc
-rm -rf bug/*.pyc
-rm -rf upload/*.pyc
-rm -rf seminar/*.pyc
-rm -rf middleware/*.pyc
-rm -rf forum/*.pyc
-
+find ./ -iname *.pyc |xargs rm -rfv
