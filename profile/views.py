e65f0b3efc1d19e0bd445c766b44f4578923469f
erenturkay <erenturkay@868bde64-912e-0410-afa2-bbf675bde9fd>
2008-02-06 20:59:25 +0000

    olay tr karakterden kaynaklaniyor, simdilik bu sekilde olsun, yarın djangoyu düzeltmeye calısacağım

--- profile/views.py
+++ profile/views.py
@@ -111,7 +111,12 @@ def user_register(request):
 
             email_subject = u"Ozgurlukicin.com Kullanıcı Hesabı, %(user)s"
             email_body = u"""Merhaba!
-%(date)s %(hour)s tarihinde %(ip_addr)s ip adresli bilgisayardan yaptığınız Ozgurlukicin.com kullanıcı hesabınızı onaylamak için aşağıdaki linke 48 saat içerisinde tıklayınız.\n\n<a href="%(link)s">%(link)s</a>\n\nTeşekkürler,\nOzgurlukicin.com"""
+%(date)s %(hour)s tarihinde %(ip_addr)s ip adresli bilgisayardan yaptiginiz Ozgurlukicin.com kullanici hesabinizi onaylamak icin lütfen asagidaki baglantiyi 48 saat icerisinde ziyaret ediniz.
+
+%(link)s
+
+Tesekkurler,
+Ozgurlukicin.com"""
 
             email_to = form.clean_data['email']
 
