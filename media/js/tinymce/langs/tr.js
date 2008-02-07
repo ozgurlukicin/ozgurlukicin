77b5d4882531088f09cb9f7708d77810f07221dc
akin <akin@868bde64-912e-0410-afa2-bbf675bde9fd>
2008-02-07 16:23:36 +0000

    tr dil dosyası

---
+++ media/js/tinymce/langs/tr.js
@@ -0,0 +1,154 @@
+tinyMCE.addI18n({en:{
+common:{
+edit_confirm:"WYSIWYG özelliğini bu yazı alanı için kullanmak ister misiniz?",
+apply:"Kabul et",
+insert:"Ekle",
+update:"Güncelle",
+cancel:"İptal",
+close:"Kapat",
+browse:"Gözat",
+class_name:"Sınıf",
+not_set:"-- Ayarlama --",
+clipboard_msg:"Kes/Kopyala/Yapıştır özellikleri Mozilla ve Firefox'da çalışmamaktadır.\n Bu konu hakkında daha fazla bilgi almak ister misiniz?",
+clipboard_no_support:"Tarayıcınız tarafından desteklenmiyor, lütfen klavye kısa yollarını kullanın.",
+popup_blocked:"Açılır pencere engelleyiciniz uygulamanın doğru çalışmasını engelliyor. Bu aracaı kullanmak için lütfen açılır pencere engelleyicinizi kapatın.",
+invalid_data:"Hata: Geçersiz değerler girildi, Bu değerler kırmızı renkle vurgulanmıştır.",
+more_colors:"Daha fazla renk"
+},
+contextmenu:{
+align:"Hizalama",
+left:"Sol",
+center:"Ortalanmış",
+right:"Sağ",
+full:"Tam"
+},
+insertdatetime:{
+date_fmt:"%d-%m-%Y",
+time_fmt:"%H:%M:%S",
+insertdate_desc:"Tarih girin",
+inserttime_desc:"Zaman girin",
+months_long:"Ocak,Şubat,Mart,Nisan,Mayıs,Haziran,Temmuz,Ağustos,Eylül,Ekim,Kasım,Aralık",
+months_short:"Ock,Şbt,Mrt,Nis,May,Haz,Tem,Ağu,Eyl,Ekm,Ksm,Arl",
+day_long:"Pazar,Pazartesi,Salı,Çarşamba,Perşembe,Cuma,Cumartesi,Pazar",
+day_short:"Pzr,Pts,Salı,Çar,Per,Cuma,Cmt,Pzr"
+},
+print:{
+print_desc:"Yazdır"
+},
+preview:{
+preview_desc:"Önüzleme"
+},
+directionality:{
+ltr_desc:"Yön: Soldan sağa",
+rtl_desc:"Yön: Sağdan sola"
+},
+layer:{
+insertlayer_desc:"Yeni katman ekle",
+forward_desc:"İleri taşı",
+backward_desc:"Geri taşı",
+absolute_desc:"Tam yeri ayarla",
+content:"Yeni katman..."
+},
+save:{
+save_desc:"Kaydet",
+cancel_desc:"Tüm değişiklikleri iptal et"
+},
+nonbreaking:{
+nonbreaking_desc:"Insert non-breaking space character"
+},
+iespell:{
+iespell_desc:"İmla kontrolü yap",
+download:"ieSpell algılanmadı. Yüklemek ister misiniz?"
+},
+advhr:{
+advhr_desc:"Horizontale rule"
+},
+emotions:{
+emotions_desc:"Gülümsemeler"
+},
+searchreplace:{
+search_desc:"Bul",
+replace_desc:"Bul/Değiştir"
+},
+advimage:{
+image_desc:"Resim düzenle/ekle "
+},
+advlink:{
+link_desc:"Link düzenle/ekle"
+},
+xhtmlxtras:{
+cite_desc:"Citation",
+abbr_desc:"Abbreviation",
+acronym_desc:"Acronym",
+del_desc:"Deletion",
+ins_desc:"Insertion",
+attribs_desc:"Insert/Edit Attributes"
+},
+style:{
+desc:"CSS Stilini düzenle"
+},
+paste:{
+paste_text_desc:"Düz metin olarak yapıştır",
+paste_word_desc:"Word dökümanından yapıştır",
+selectall_desc:"Hepsini Seç"
+},
+paste_dlg:{
+text_title:"Use CTRL+V on your keyboard to paste the text into the window.",
+text_linebreaks:"Keep linebreaks",
+word_title:"Use CTRL+V on your keyboard to paste the text into the window."
+},
+table:{
+desc:"Inserts a new table",
+row_before_desc:"Insert row before",
+row_after_desc:"Insert row after",
+delete_row_desc:"Delete row",
+col_before_desc:"Insert column before",
+col_after_desc:"Insert column after",
+delete_col_desc:"Remove column",
+split_cells_desc:"Split merged table cells",
+merge_cells_desc:"Merge table cells",
+row_desc:"Table row properties",
+cell_desc:"Table cell properties",
+props_desc:"Table properties",
+paste_row_before_desc:"Paste table row before",
+paste_row_after_desc:"Paste table row after",
+cut_row_desc:"Cut table row",
+copy_row_desc:"Copy table row",
+del:"Delete table",
+row:"Row",
+col:"Column",
+cell:"Cell"
+},
+autosave:{
+unload_msg:"Eğer bu sayfadan ayrılırsanız yaptığınız tüm değişiklikler kaybolacaktır."
+},
+fullscreen:{
+desc:"Tam ekran moduna geç"
+},
+media:{
+desc:"Medya düzenle / ekle",
+edit:"Gömülmüş medyayı düzenle"
+},
+fullpage:{
+desc:"Döküman özellikleri"
+},
+template:{
+desc:"Önceden belirlenmiş taslak içeriğini yapıştır"
+},
+visualchars:{
+desc:"Visual control characters on/off."
+},
+spellchecker:{
+desc:"İmla kontrolü yap",
+menu:"İmla kontrolü ayarları",
+ignore_word:"Kelimeyi gözardı et",
+ignore_words:"Hepsini gözardı et",
+langs:"Diller",
+wait:"Lütfen bekleyin...",
+sug:"Öneriler",
+no_sug:"Öneri yok",
+no_mpell:"Hatalı kelimeye rastlanmadı."
+},
+pagebreak:{
+desc:"Sayfa ayracı ekle."
+}}});
