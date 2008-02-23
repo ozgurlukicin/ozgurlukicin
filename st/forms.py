9e78921ed88880377b76207beb70fbf02b3ec08d
ahmet <ahmet@868bde64-912e-0410-afa2-bbf675bde9fd>
2008-02-23 14:11:33 +0000

    Farklı bir filtre arayışına giriyoruz...

--- st/forms.py
+++ st/forms.py
@@ -4,7 +4,6 @@ import re
 class SearchForm(forms.Form):
     term = forms.CharField(label='Anahtar kelime', required=True, widget=forms.TextInput(attrs={'size': '40',}))
 
-
 class XssField(forms.CharField):
     """ That one will validate the screen upload things"""
 
@@ -12,21 +11,6 @@ class XssField(forms.CharField):
         """ Calling the upper function"""
         super(XssField,self).__init__(*args,**kwargs)
 
-    def clean(self,value):
-        #for filtering malicious code
-        from oi.forum.stripogram import html2text,html2safehtml
-
-        if self.required and not value:
-            raise forms.ValidationError(_(u'Bos birakilamaz'))
-
-        #strip the tags we dont want
-        value=html2safehtml(value,valid_tags=('a','b','i','li','img','ul', 'h1', 'h2', 'h3', 'h4', 'h5','title'))
-
-        if not value:
-            raise forms.ValidationError(_(u'Xss mi lutfen ama !'))
-
-        return value
-
 class CommentForm(forms.Form):
-    """ The comment thngyy add validation please..."""
+    """ The comment thingy add validation please..."""
     yorum=XssField(label="Yorum",required=True,max_length=100,widget=forms.Textarea(attrs={'rows': '20', 'cols': '60',}))
