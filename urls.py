0d68454e603a66d81e2047ad526ef2eafe4bdf32
jnmbk <jnmbk@868bde64-912e-0410-afa2-bbf675bde9fd>
2008-03-21 12:59:30 +0000

    removed unnecessary lines, and converted tabs to spaces

--- urls.py
+++ urls.py
@@ -100,11 +100,9 @@ urlpatterns = patterns('',
 
     (r'^robots.txt$', 'oi.st.views.robots'),
 
-	#comments
-
+    #comments
     (r'^comments/post/$', 'oi.comments.views.post_comment'),
     (r'^comments/posted/$', 'oi.comments.views.comment_was_posted'),
-
 
     #News
     #(r'^haber/yorum/(?P<id>\d+)/$', 'oi.st.views.comment_news'),
@@ -112,7 +110,6 @@ urlpatterns = patterns('',
     (r'^haber/$', 'django.views.generic.list_detail.object_list', dict(news_dict)),
     (r'^haber/(?P<slug>.*)/yazdir/$', 'oi.st.views.news_printable'),
     (r'^haber/(?P<slug>.*)/$', 'oi.st.views.news_detail'),
-
 
     #Packages
     (r'^paket/yorum/(?P<slug>.*)/$', 'oi.st.views.comment_package'),
