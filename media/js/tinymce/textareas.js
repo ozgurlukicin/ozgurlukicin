cc79dfca127d8a9a91cec692436462f8bac8ed74
jnmbk <jnmbk@868bde64-912e-0410-afa2-bbf675bde9fd>
2008-04-22 11:59:43 +0000

    we use gecko in admin... so let's enable spellcheck and remove unneeded things

--- media/js/tinymce/textareas.js
+++ media/js/tinymce/textareas.js
@@ -1,7 +1,7 @@
 tinyMCE.init({
     mode : "textareas",
     theme : "advanced",
-    theme_advanced_buttons1 : "copy,paste,undo,redo,charmap,search,replace,|,link,unlink,image,emotions,|,code",
+    theme_advanced_buttons1 : "undo,redo,charmap,search,replace,|,link,unlink,image,emotions,|,code",
     theme_advanced_buttons2 : "formatselect,fontsizeselect,bold,italic,underline,strikethrough,sub,sup,|,justifyleft,justifycenter,justifyright,justifyfull,outdent,indent,|,bullist,numlist,|,forecolor,backcolor,removeformat",
     theme_advanced_buttons3 : "",
     theme_advanced_toolbar_location : "top",
@@ -17,7 +17,8 @@ tinyMCE.init({
     extended_valid_elements : "a[name|href|target|title],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|name],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]",
     advimage_update_dimensions_onchange: true,
     file_browser_callback : 'oiFileBrowser',
-    plugins : "advimage,autosave,contextmenu,searchreplace,table,visualchars,advlink,emotions,media,style,template,xhtmlxtras",
+    plugins : "advimage,autosave,searchreplace,table,visualchars,advlink,emotions,media,style,template,xhtmlxtras",
+    gecko_spellcheck : true,
     language : "tr",
 });
 
