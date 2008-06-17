a1ca47cb05e81f9d22e07d14fe253bbf123d8f05
jnmbk <jnmbk@868bde64-912e-0410-afa2-bbf675bde9fd>
2008-06-17 17:41:14 +0000

    init for poll

---
+++ poll/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008 Uğur Çetin
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
