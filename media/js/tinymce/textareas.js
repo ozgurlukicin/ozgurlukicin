e6e2019b62f7999640f2e194652a998b1e046503
jnmbk <jnmbk@868bde64-912e-0410-afa2-bbf675bde9fd>
2008-08-04 18:46:49 +0000

    only merging tinymce fix now, video needs more love before merge

--- media/js/tinymce/textareas.js
+++ media/js/tinymce/textareas.js
@@ -14,7 +14,7 @@ tinyMCE.init({
     convert_urls : false,
     width : "800",
     height : "300",
-    extended_valid_elements : "a[name|href|target|title],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|name|sytle],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]",
+    extended_valid_elements : "a[name|href|target|title],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|name|style],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]",
     advimage_update_dimensions_onchange: true,
     file_browser_callback : 'oiFileBrowser',
     plugins : "advimage,autosave,searchreplace,table,visualchars,advlink,emotions,media,style,template,xhtmlxtras",
