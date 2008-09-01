f542dc44bae81ee71d8b8633957600f0359143db
huseyin.berberoglu <huseyin.berberoglu@868bde64-912e-0410-afa2-bbf675bde9fd>
2008-09-01 01:10:06 +0000

    vote button works true now.

--- media/js/brainstorm.js
+++ media/js/brainstorm.js
@@ -13,18 +13,32 @@ function favori(id, proc) {
         });
 }
 function oyla(id, vote) {
-      $.get("/yenifikir/oyla/" + id + "/" + vote + "/",
-        function(data) {
-            $('#oyla' + id).hide()
-            if (data.substr(0,2) == "OK") {
-                data = data.replace(/OK/, "")
-                $('#vote' + id).html(data);
-                $("#img_" + id + "_" + vote).parent().attr("href", 'test')
-                $("#img_" + id + "_" + vote).attr("src", "/media/img/new/ideas_voted_" + vote + ".png");
-            } else {
-                document.location = '/kullanici/giris/';
-            }
-        });
+        img = $('#img_' + id + '_' + vote);
+        if (img.hasClass("voted")) {
+            img.removeClass("voted");
+            img.attr("src", "/media/img/new/ideas_vote_" + vote + ".png");
+            oyiptal(id, vote);
+            if (vote == 1) { vote = 0; } else { vote = 1; }
+            $('#img_' + id + '_' + vote).removeClass("cursor_default");
+            $('#img_' + id + '_' + vote).attr("disabled","");
+        } else {
+            img.addClass("voted");
+            $.get("/yenifikir/oyla/" + id + "/" + vote + "/",
+                function(data) {
+                    $('#oyla' + id).hide()
+                    if (data.substr(0,2) == "OK") {
+                        data = data.replace(/OK/, "")
+                        $('#vote' + id).html(data);
+                        img.parent().attr("href", 'test')
+                        img.attr("src", "/media/img/new/ideas_voted_" + vote + ".png");
+                    if (vote == 1) { vote = 0; } else { vote = 1; }
+                    $('#img_' + id + '_' + vote).addClass("cursor_default");
+                    $('#img_' + id + '_' + vote).attr("disabled","disabled");
+                } else {
+                    document.location = '/kullanici/giris/';
+                }
+            });
+        }
 }
 function oyiptal(id, vote) {
     $.get("/yenifikir/oyiptal/" + id,
