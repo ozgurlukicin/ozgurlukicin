04e7b88616bd92863bf4b2532152c2fb4826e29c
huseyin.berberoglu <huseyin.berberoglu@868bde64-912e-0410-afa2-bbf675bde9fd>
2008-09-02 09:50:45 +0000

    less code is better

--- media/js/home.js
+++ media/js/home.js
@@ -1,41 +1,15 @@
 $(document).ready(function() {
             setInterval(function () {
-                if ($("#news_count").html() == '5') {
-                    goFirst();
-                } else {
-                        next_news()
-                }
+                change_news(1)
             }, 10000)
         });
 
-function next_news() {
-    $(".active").hide();
-    $("#news_count").html(parseInt($("#news_count").html()) + 1);
-    if ($("#news_count").html() == '6') goFirst();
-    $(".active").next().addClass("active");
-    $(".active:first").removeClass("active");
-    $(".active").fadeIn();
-}
-function prev_news() {
-    $("#news_count").html(parseInt($("#news_count").html()) - 1);
-    if ($("#news_count").html() == '') goLast();
-    $(".active").hide();
-    $(".active").prev().addClass("active");
-    $(".active:last").removeClass("active");
-    $(".active").fadeIn();
-}
-function goFirst() {
-    $("#news_count").html('1');
-    $(".active").hide();
-    $(".active").removeClass("active");
-    $(".news_container:first").addClass("active");
-    $(".active").fadeIn();
-}
-
-function goLast() {
-    $("#news_count").html('5');
-    $(".news_container").hide();
-    $(".active").removeClass("active");
-    $(".news_container:last").addClass("active");
-    $(".active").fadeIn();
+function change_news(direction) {
+    nc = $("#news_count").html();
+    $("#news_" + nc).hide();
+    if (nc == '1' && direction == -1) nc = 6;
+    if (nc == '5' && direction == 1) nc = 0;
+    nc = parseInt(nc) + direction;
+    $("#news_" + nc).fadeIn();
+    $("#news_count").html(nc);
 }
