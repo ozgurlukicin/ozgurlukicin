79263bc333bf1c54407bac2bfbc4a6c76831edb0
erenturkay <erenturkay@868bde64-912e-0410-afa2-bbf675bde9fd>
2008-09-02 12:35:41 +0000

    * add tooltip support to menu * jquery.js is in base.html and forum_base.html as it's used in everywhere now * add jquery.tooltip.chilipack.{js, css} into menu.html as it's not used in everywhere - in menu.html, there should be more title attirbute to be shown as tooltip. These are just examples ;)

--- media/js/home.js
+++ media/js/home.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(function() {
             setInterval(function () {
                 change_news(1)
             }, 10000)
---
+++ media/js/menu_tooltip.js
@@ -0,0 +1,12 @@
+//
+// Js for tooltip on everypage, see menu.html
+//
+
+$(function(){
+        $("#menu a").tooltip({
+            delay: 500,
+            track: true,
+            showURL: false,
+            fade: 350
+        });
+    });
