8500297eff8ba6522a439e467519f6fe3fe323d7
jnmbk <jnmbk@868bde64-912e-0410-afa2-bbf675bde9fd>
2008-09-29 11:12:46 +0000

    RSS and atom feeds for ideas

---
+++ ideas/feeds.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008 Artistanbul
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from django.contrib.syndication.feeds import Feed
+from django.utils.feedgenerator import Atom1Feed
+
+from oi.settings import SITE_NAME, WEB_URL, SITE_DESC
+
+from django.contrib.syndication.feeds import FeedDoesNotExist
+from oi.ideas.models import Idea
+
+class RSS(Feed):
+    title = SITE_NAME + " Beyin Fırtınası"
+    link = WEB_URL
+    description = SITE_DESC
+
+    def items(self):
+        objects = Idea.objects.filter(is_hidden=False).order_by('-submitted_date')[:40]
+        return objects
+
+    def item_author_name(self, item):
+        return item.submitter
+
+    def item_pubdate(self, item):
+        return item.submitted_date
+
+    def item_categories(self, item):
+        return [tag.name for tag in item.tags.all()]
+
+class Atom(RSS):
+    feed_type = Atom1Feed
+    subtitle = RSS.description
--- ideas/urls.py
+++ ideas/urls.py
@@ -1,15 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008 Artistanbul
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
 from django.conf.urls.defaults import *
+from oi.ideas.feeds import RSS, Atom
+
+rss_dict = { "feed": RSS }
+atom_dict = { "feed": Atom }
 
 urlpatterns = patterns('oi.ideas.views',
-                       (r'^$', 'list'),
-                       (r'^ekle/$', 'add'),
-                       (r'^duzenle/(?P<idea_id>.*)/$', 'edit_idea'),
-                       (r'^sil/(?P<idea_id>.*)/$', 'delete_idea'),
-                       (r'^oyla/$', 'vote'),
-                       (r'^ayrinti/(?P<idea_id>.*)/$', 'detail'),
-                       (r'^ayrinti/(?P<idea_id>.*)/favori/ekle$', 'add_favorite'),
-                       (r'^ayrinti/(?P<idea_id>.*)/favori/cikar$', 'del_favorite'),
-                       (r'^listele/(?P<field>.*)/(?P<filter_slug>.*)/$', 'list'),
-                       (r'^tekrar/(?P<idea_id>.*)/(?P<duplicate_id>.*)/$', 'duplicate'),
-                       (r'^durumdegistir/((?P<idea_id>.*))/(?P<new_status>.*)/$', 'change_status'),
+        (r'^$', 'list'),
+        (r'^ekle/$', 'add'),
+        (r'^duzenle/(?P<idea_id>.*)/$', 'edit_idea'),
+        (r'^sil/(?P<idea_id>.*)/$', 'delete_idea'),
+        (r'^oyla/$', 'vote'),
+        (r'^ayrinti/(?P<idea_id>.*)/$', 'detail'),
+        (r'^ayrinti/(?P<idea_id>.*)/favori/ekle$', 'add_favorite'),
+        (r'^ayrinti/(?P<idea_id>.*)/favori/cikar$', 'del_favorite'),
+        (r'^listele/(?P<field>.*)/(?P<filter_slug>.*)/$', 'list'),
+        (r'^tekrar/(?P<idea_id>.*)/(?P<duplicate_id>.*)/$', 'duplicate'),
+        (r'^durumdegistir/((?P<idea_id>.*))/(?P<new_status>.*)/$', 'change_status'),
+)
+urlpatterns += patterns('',
+        (r'^rss/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': rss_dict}),
+        (r'^atom/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': atom_dict}),
 )
