a278008b37c5a6bf533e17cb8610d464ab1ce6d2
jnmbk <jnmbk@868bde64-912e-0410-afa2-bbf675bde9fd>
2008-11-02 14:39:22 +0000

    better sidebar for shop

--- shop/product/templatetags/shop_library.py
+++ shop/product/templatetags/shop_library.py
@@ -24,7 +24,7 @@ def recurse_for_children(current_node, parent_node, active_cat, show_empty=True)
         if current_node.parent:
              link = SubElement(temp_parent, 'a', attrs)
         else:
-             link = SubElement(temp_parent, 'p', {})
+             link = SubElement(temp_parent, 'span', {})
         link.text = current_node.name
 
         if child_count > 0:
