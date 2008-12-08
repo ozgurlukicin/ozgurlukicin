28b6bb86c210cf694816cfafb520bba5cb5f889c
erenturkay <erenturkay@868bde64-912e-0410-afa2-bbf675bde9fd>
2008-12-08 18:01:42 +0000

    * update header

--- st/templatetags/gravatar.py
+++ st/templatetags/gravatar.py
@@ -1,4 +1,9 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
+#
+# Copyright 2008 Artistanbul
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from django.template import Library
 from django.conf import settings
