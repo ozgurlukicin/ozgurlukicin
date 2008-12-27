65e936659f5fb5a3cf0e6baf037e1a08c08a09d4
jnmbk <jnmbk@868bde64-912e-0410-afa2-bbf675bde9fd>
2008-12-27 16:24:10 +0000

    proper gravatar implementation from django snippets

--- st/templatetags/gravatar.py
+++ st/templatetags/gravatar.py
@@ -1,113 +1,39 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-#
-# Copyright 2008 Artistanbul
-# Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/copyleft/gpl.txt.
 
+from hashlib import md5
+
+from django import template
 from django.template import Library
-from django.conf import settings
 
-from urlgrabber.grabber import urlopen, URLGrabError
-from stat import *
-import md5
-import os
-import time
+# We call it avatar_url instead of gravatar_url so if we change services in the
+# future it's just a change of the tag.
 
-GRAVATAR_URL = "http://www.gravatar.com/avatar"
-GRAVATAR_SERVER_TIMEOUT = 2
-
-# We will check if user has gravatar account according to this hash
-# NOTE:
-# This hash is for 100px default image. If you happen to use different resolution by default, you should change the hash.
-# We're using 100px avatars by default.
-GRAVATAR_DEFAULT_IMAGE_HASH = "e2deb22899972ac721b862f9c8e73ef1"
-
-# the path cache files are stored.
-CACHE_PATH = "%s/tmp" % settings.DOCUMENT_ROOT
-# cache suffix, for look&feel thingy :) I don't the way when only hashes are listed when I look at tmp dir :P
-CACHE_SUFFIX = "gravatar"
-# Our cache is valid for 1.5 hours
-CACHE_TIMEOUT = 5400
-
-class Gravatar:
-    """
-    # Caching stuff:
-    # 1- Check if we already checked out user's gravatar
-    # 2- If the file exists, look up its content. It will either 1 or 0 so as to explain whether it's default gravatar image or not.
-    # 3- If doesn't exist, check out the avatar and write the hash of user's email as filename
-    # 4- If it's default gravatar image (content is 0), just return user's avatar on the site
-    # 5- If content is 1, return gravatar URL directly,
-    # 6- Finally, check the file's creation date. If 1 hour lasted, delete it. This means our cache machanism last for 1 hour :)
-    """
-    def __init__(self):
-        self.cache_file = None
-
-    # creates cache and writes the data in it.
-    def create_cache(self, data):
-        file = open(self.cache_file, "w")
-        file.write(data)
-        file.close()
-
-    def is_cached(self):
-        """Check if the user's avatar is cached"""
-        if os.path.exists(self.cache_file):
-            return True
-        else:
-            return False
-
-    def is_cache_expired(self):
-        """We keep our cache valid for 1.5 hours."""
-        last_modified = os.stat(self.cache_file)[ST_MTIME]
-
-        if (time.time() - last_modified) > CACHE_TIMEOUT:
-            return True
-        else:
-            return False
-
-    def has_gravatar_account(self):
-        if open(self.cache_file).read() == "1":
-            return True
-        else:
-            return False
-
-    # It's like a constructor.
-    def get_gravatar_image(self, userObject, image_size=100):
-        email_hash = md5.md5(userObject.email).hexdigest()
-        self.cache_file = "%s/%s-%s" % (CACHE_PATH, CACHE_SUFFIX, email_hash)
-        gravatar_image_url = "%s/%s?s=%s" % (GRAVATAR_URL, email_hash, image_size)
-
-        # have we checked the image for user already?
-        if not self.is_cached():
-            try:
-                connection = urlopen(gravatar_image_url, timeout=GRAVATAR_SERVER_TIMEOUT)
-                image = connection.read()
-                connection.close()
-
-                # if it's default image, write 0 to cache file
-                if md5.md5(image).hexdigest() == GRAVATAR_DEFAULT_IMAGE_HASH:
-                    self.create_cache("0")
-                    return userObject.get_profile().avatar.file.url
-                else:
-                    self.create_cache("1")
-                    return gravatar_image_url
-            except URLGrabError:
-                # gravatar connection error
-                self.create_cache("0")
-                return userObject.get_profile().avatar.file.url
-        else:
-            # if our cache is expired, delete the file and recall this function.
-            if self.is_cache_expired():
-                os.remove(self.cache_file)
-                self.get_gravatar_image(userObject, image_size)
-
-            # if it's cached and the user has gravatar account, return gravatar URL.
-            # if not, you know what to do :)
-            if self.has_gravatar_account():
-                return gravatar_image_url
-            else:
-                return userObject.get_profile().avatar.file.url
-
-gravatar = Gravatar()
 register = Library()
-register.simple_tag(gravatar.get_gravatar_image)
+
+@register.simple_tag
+def avatar_url(email, size=50, rating='g', default=None):
+    """
+    Returns a gravatar url.
+
+    Example tag usage: {% avatar user.email 80 "g" %}
+
+    :Parameters:
+       - `email`: the email to send to gravatar.
+       - `size`: optional YxY size for the image.
+       - `rating`: optional rating (g, pg, r, or x) of the image.
+       - `default`: optional default image url or hosted image like wavatar.
+    """
+    # Verify the rating actually is a rating accepted by gravatar
+    rating = rating.lower()
+    ratings = ['g', 'pg', 'r', 'x']
+    if rating not in ratings:
+        raise template.TemplateSyntaxError('rating must be %s' % (
+            ", ".join(ratings)))
+    # Create and return the url
+    hash = md5(email).hexdigest()
+    url = 'http://www.gravatar.com/avatar/%s?s=%s&r=%s' % (
+        hash, size, rating)
+    if default:
+        url = "%s&d=%s" % (url, default)
+    return url
