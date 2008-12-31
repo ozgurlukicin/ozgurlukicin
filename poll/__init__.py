3b68224c7680fd5f669ced5712d5c22da6f85beb
jnmbk <jnmbk@868bde64-912e-0410-afa2-bbf675bde9fd>
2008-12-31 13:40:47 +0000

    fix copyright

--- poll/__init__.py
+++ poll/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2008 Uğur Çetin
+# Copyright 2008 Artistanbul
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
