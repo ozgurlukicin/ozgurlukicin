cab2348fcb6cbb914f0b18367e7c0e9e81dceb72
jnmbk <jnmbk@868bde64-912e-0410-afa2-bbf675bde9fd>
2008-12-31 11:25:58 +0000

    seminar pages

--- seminar/urls.py
+++ seminar/urls.py
@@ -7,17 +7,8 @@
 
 from django.conf.urls.defaults import patterns
 
-from oi.settings import SEMINAR_PER_PAGE
-
 from oi.seminar.models import Seminar
 from oi.profile.settings import googleMapsApiKey
-
-seminar_list = {
-                'queryset': Seminar.objects.filter(status=True),
-                'template_name': 'seminar/seminar.html',
-                'paginate_by': SEMINAR_PER_PAGE,
-                'template_object_name': 'seminar'
-               }
 
 seminar = {
         "queryset": Seminar.objects.filter(status=True),
@@ -25,8 +16,5 @@ seminar = {
         }
 
 urlpatterns = patterns('',
-    (r'^liste/$', 'django.views.generic.list_detail.object_list', dict(seminar_list, page=1)),
-    (r'^sayfa/(?P<page>[0-9]+)/$', 'django.views.generic.list_detail.object_list', dict(seminar_list)),
     (r'^(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail', dict(seminar)),
-    (r'^yer/(?P<place>\d+)/$', 'oi.seminar.views.place'),
 )
--- seminar/views.py
+++ seminar/views.py
@@ -4,11 +4,3 @@
 # Copyright 2007 Artistanbul
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
-
-from django.shortcuts import get_object_or_404
-
-from oi.seminar.models import Place, Attender, Seminar
-from oi.st.wrappers import render_response
-
-def place(request, slug):
-    return render_response(request, 'seminar/place.html', locals())
