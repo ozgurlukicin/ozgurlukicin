797045b3678f07b9acd90f4c03426231a42347a7
jnmbk <jnmbk@868bde64-912e-0410-afa2-bbf675bde9fd>
2009-01-22 14:48:45 +0000

    flatpage models

--- flatpages/models.py
+++ flatpages/models.py
@@ -9,19 +9,20 @@ from django.db import models
 from django.contrib.auth.models import User
 from oi.st.tags import Tag
 from oi.middleware import threadlocals
+from django.utils.translation import ugettext as _
 
 class FlatPage(models.Model):
-    url = models.CharField('URL', max_length=100)
-    title = models.CharField('Başlık', max_length=200)
-    text = models.TextField('Metin')
+    url = models.CharField(_("URL"), max_length=100)
+    title = models.CharField(_("Title"), max_length=200)
+    text = models.TextField(_("Text"))
     author = models.ForeignKey(User, blank=True, editable=False)
     tags = models.ManyToManyField(Tag)
-    update = models.DateTimeField('Tarih', blank=False)
-    template_name = models.CharField('Şablon adı', max_length=70, blank=True)
+    update = models.DateTimeField(_("Date"), blank=False)
+    template_name = models.CharField(_("Template name"), max_length=70, blank=True)
 
     class Meta:
-        verbose_name = 'Statik sayfa'
-        verbose_name_plural = 'Statik sayfalar'
+        verbose_name = _("Static page")
+        verbose_name_plural = _("Static pages")
 
     def __unicode__(self):
         return "%s -- %s" % (self.url, self.title)
