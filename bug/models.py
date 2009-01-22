d039037d74c8ac26f5b6d72327cf9bbb4acf2a2e
jnmbk <jnmbk@868bde64-912e-0410-afa2-bbf675bde9fd>
2009-01-22 13:03:25 +0000

    bug models and a fix

--- bug/models.py
+++ bug/models.py
@@ -9,28 +9,29 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
 from oi.settings import WEB_URL
+from django.utils.translation import ugettext as _
 
 STATUS_CODES = (
-    (1, "Yeni"),
-    (2, "Atandı"),
-    (3, "Kapalı"),
+    (1, _("New")),
+    (2, _("Assigned")),
+    (3, _("Closed")),
     )
 PRIORITIES = (
-    (1, "Engelleyici"),
-    (2, "Büyük"),
-    (3, "Normal"),
-    (4, "Küçük"),
-    (5, "İyileştirme"),
+    (1, _("Preventive")),
+    (2, _("Important")),
+    (3, _("Normal")),
+    (4, _("Unimportant")),
+    (5, _("Improvement")),
     )
 
 class Bug(models.Model):
-    title = models.CharField(verbose_name="Başlık", max_length=100)
-    submitted_date = models.DateTimeField(verbose_name="Tarih", auto_now_add=True)
-    submitter = models.ForeignKey(User, verbose_name="Gönderen", related_name="submitter")
-    assigned_to = models.ForeignKey(User, verbose_name="Atanan")
-    description = models.TextField(verbose_name="Tanım", blank=False)
-    status = models.IntegerField(verbose_name="Durum", default=1, choices=STATUS_CODES)
-    priority = models.IntegerField(verbose_name="Öncelik", default=3, choices=PRIORITIES)
+    title = models.CharField(verbose_name=_("Title"), max_length=100)
+    submitted_date = models.DateTimeField(verbose_name=_("Date"), auto_now_add=True)
+    submitter = models.ForeignKey(User, verbose_name=_("Sender"), related_name="submitter")
+    assigned_to = models.ForeignKey(User, verbose_name=_("Assignee"))
+    description = models.TextField(verbose_name=_("Description"), blank=False)
+    status = models.IntegerField(verbose_name=_("Status"), default=1, choices=STATUS_CODES)
+    priority = models.IntegerField(verbose_name=_("Priority"), default=3, choices=PRIORITIES)
 
     def __unicode__(self):
         return self.title
@@ -47,10 +48,10 @@ class Bug(models.Model):
         search_fields = ('assigned_to', 'submitter', 'title', 'description')
 
 class Comment(models.Model):
-    bug = models.ForeignKey(Bug, verbose_name="Hata")
-    author = models.ForeignKey(User, verbose_name="Yazan")
-    text = models.TextField(blank=False, verbose_name="Yorum")
-    date = models.DateTimeField(auto_now_add=True, verbose_name="Tarih")
+    bug = models.ForeignKey(Bug, verbose_name=_("Bug"))
+    author = models.ForeignKey(User, verbose_name=_("Author"))
+    text = models.TextField(blank=False, verbose_name=_("Comment"))
+    date = models.DateTimeField(auto_now_add=True, verbose_name=_("Date"))
 
     def __unicode__(self):
         return self.text
