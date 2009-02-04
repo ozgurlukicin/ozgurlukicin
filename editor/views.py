1cfb45328bbee008a63dcb9f4670d71802c9a54c
jnmbk <jnmbk@868bde64-912e-0410-afa2-bbf675bde9fd>
2009-02-04 05:52:34 +0000

    create news

--- editor/views.py
+++ editor/views.py
@@ -5,14 +5,43 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
+import datetime
+
 from django.contrib.auth.decorators import permission_required
 
 from oi.editor.forms import ContributedNewsForm
+from oi.editor.models import ContributedNews
 from oi.st.wrappers import render_response
+from oi.st.models import News
 
 @permission_required('editor.change_contributednews', login_url="/kullanici/giris/")
 def create_contributednews(request):
-    form = ContributedNewsForm()
+    if request.method == 'POST':
+        form = ContributedNewsForm(request.POST.copy())
+        if form.is_valid():
+            news = News(
+                    title = form.cleaned_data["title"],
+                    slug = form.cleaned_data["slug"],
+                    image = form.cleaned_data["image"],
+                    sum = form.cleaned_data["sum"],
+                    text = form.cleaned_data["text"],
+                    tags = form.cleaned_data["tags"],
+                    update = datetime.datetime.now(),
+                    author = form.cleaned_data["author"],
+                    status = False,
+                    )
+            news.save()
+            contributedNews = ContributedNews(
+                    news = news,
+                    contributor = request.user,
+                    )
+            contributedNews.save()
+            return HttpResponseRedirect(news.get_absolute_url())
+        else:
+            return render_response(request, "editor/create.html", locals())
+    else:
+        form = ContributedNewsForm()
+        return render_response(request, "editor/create.html", locals())
 
 @permission_required('editor.create_contributednews', login_url="/kullanici/giris/")
 def change_contributednews(request):
