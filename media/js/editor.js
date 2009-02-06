1f21f920877c1420b8e95d3c55d0d4ed662ee5c3
jnmbk <jnmbk@868bde64-912e-0410-afa2-bbf675bde9fd>
2009-02-06 14:47:16 +0000

    image selector completed

--- media/js/editor.js
+++ media/js/editor.js
@@ -1,3 +1,9 @@
+$(document).ready(function() {
+    $("#id_image").attr("onchange", "update_image(this)");
+    $("#id_image").attr("onkeyup", "update_image(this)");
+    $("#filter").attr("onchange", "filter_images()");
+    $("#filter").attr("onkeyup", "filter_images()");
+});
 function filter_images() {
     if ($("#filter").val().length > 2) {
         $("#id_image > option").hide();
@@ -8,5 +14,11 @@ function filter_images() {
         } else {
             $("#filter").css("color", "");
         }
+    } else {
+        $("#id_image > option").show();
+        $("#filter").css("color", "");
     }
+}
+function update_image(selectBox) {
+    $("#image_thumb").attr("src", "/media/" + selectBox[selectBox.selectedIndex].innerHTML);
 }
