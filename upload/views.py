9f0450ce53d8f2bfe2f65d98e23118656ce30759
jnmbk <jnmbk@868bde64-912e-0410-afa2-bbf675bde9fd>
2009-02-16 11:13:34 +0000

    change upload permissions for editors

--- upload/views.py
+++ upload/views.py
@@ -7,26 +7,23 @@
 
 from os import path
 
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import permission_required
 from django import forms
 from django.shortcuts import render_to_response
 from oi.upload.models import Image, ImageUploadForm
 from oi.settings import MEDIA_URL, MEDIA_ROOT
 
-@login_required
+@permission_required('editor.create_contributednews', login_url="/kullanici/giris/")
 def image_upload(request):
-    if request.user.is_staff:
-        if request.method == 'POST':
-            form = ImageUploadForm(request.POST, request.FILES)
-            if form.is_valid():
-                f = request.FILES['file']
-                dest = open('%s/upload/image/%s' % (MEDIA_ROOT, f.name), 'wb+')
-                for chunk in f.chunks():
-                    dest.write(chunk)
-                dest.close()
-                return render_to_response('file_upload/file_upload_success.html', { 'filename': f.name})
-        else:
-            form = ImageUploadForm()
-            return render_to_response('file_upload/file_upload.html', {'form': form})
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            f = request.FILES['file']
+            dest = open('%s/upload/image/%s' % (MEDIA_ROOT, f.name), 'wb+')
+            for chunk in f.chunks():
+                dest.write(chunk)
+            dest.close()
+            return render_to_response('file_upload/file_upload_success.html', { 'filename': f.name})
     else:
-        return render_to_response('home.html')
+        form = ImageUploadForm()
+        return render_to_response('file_upload/file_upload.html', {'form': form})
