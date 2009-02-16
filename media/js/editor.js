11865ce963307f6984ba7d01a12f83a9971493f0
jnmbk <jnmbk@868bde64-912e-0410-afa2-bbf675bde9fd>
2009-02-16 11:32:37 +0000

    customized tinymce for editors

--- media/js/editor.js
+++ media/js/editor.js
@@ -27,3 +27,43 @@ function filter_images() {
 function update_image(selectBox) {
     $("#image_thumb").attr("src", "/media/" + selectBox[selectBox.selectedIndex].innerHTML);
 }
+tinyMCE.init({
+    mode : "textareas",
+    theme : "advanced",
+    theme_advanced_buttons1 : "undo,redo,charmap,search,replace,|,link,unlink,image,emotions,|,code,fullscreen",
+    theme_advanced_buttons2 : "formatselect,fontsizeselect,bold,italic,underline,strikethrough,sub,sup,|,justifyleft,justifycenter,justifyright,justifyfull,outdent,indent,|,bullist,numlist,|,forecolor,backcolor,removeformat",
+    theme_advanced_buttons3 : "",
+    theme_advanced_toolbar_location : "top",
+    theme_advanced_toolbar_align : "left",
+    theme_advanced_path_location : "bottom",
+    theme_advanced_resizing : true,
+    browsers : "msie,gecko,opera,safari",
+    dialog_type : "modal",
+    entity_encoding : "raw",
+    relative_urls : false,
+    convert_urls : false,
+    width : "783",
+    height : "300",
+    extended_valid_elements : "a[name|href|target|title],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|name|style],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]",
+    advimage_update_dimensions_onchange: true,
+    file_browser_callback : 'oiFileBrowser',
+    plugins : "advimage,autosave,searchreplace,table,visualchars,advlink,emotions,media,style,template,xhtmlxtras,fullscreen",
+    gecko_spellcheck : true,
+    language : "tr"
+});
+
+function oiFileBrowser (field_name, url, type, win) {
+    tinyMCE.activeEditor.windowManager.open({
+        file : "/admin/upload/image/tinymce/",
+        title : "File Browser",
+        width : 300,
+        height : 100,
+        close_previous : "no"
+    }, {
+        window : win,
+        input : field_name,
+        resizable : "yes",
+        inline : "yes",
+    });
+    return false;
+}
