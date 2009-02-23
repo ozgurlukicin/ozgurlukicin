52de10248b7cb688005a63d9daf884a1642782bd
jnmbk <jnmbk@868bde64-912e-0410-afa2-bbf675bde9fd>
2009-02-23 14:58:06 +0000

    image filter and display in article admin pages

---
+++ media/js/adminimages.js
@@ -0,0 +1,28 @@
+function update_image(selectBox) {
+    $("#image_thumb").attr("src", "/media/" + selectBox[selectBox.selectedIndex].innerHTML);
+}
+$(document).ready(function() {
+    var image = $("#id_image");
+    image.attr("onchange", "update_image(this)");
+    image.attr("onkeyup", "update_image(this)");
+    $(".image").before("<div class=\"form-row image_filter\"><div><label for=\"image_filter\">Görselleri Filtrele</label><input type=\"text\" maxlength=\"32\" name=\"image_filter\" class=\"vTextField\" id=\"filter\"/><p class=\"help\">Burayı kullanarak görsellerde arama yapabilirsiniz.</p></div></div>");
+    $(".image > div").after("<img src=\"\" id=\"image_thumb\" alt=\"Üstteki arama kutusunu kullanıp listeden bir görsel seçiniz\" />");
+    $("#filter").attr("onchange", "filter_images()");
+    $("#filter").attr("onkeyup", "filter_images()");
+    update_image(document.getElementById("id_image"));
+});
+function filter_images() {
+    if ($("#filter").val().length > 2) {
+        $("#id_image > option").hide();
+        var results =  $("#id_image > option:contains('" + $("#filter").val() + "')")
+        results.show();
+        if (results.length == 0) {
+            $("#filter").css("color", "red");
+        } else {
+            $("#filter").css("color", "");
+        }
+    } else {
+        $("#id_image > option").show();
+        $("#filter").css("color", "");
+    }
+}
--- st/admin.py
+++ st/admin.py
@@ -28,7 +28,7 @@ class ArticleAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ("title",)}
 
     class Media:
-        js = ("js/tinymce/tiny_mce.js", "js/tinymce/textareas.js",)
+        js = ("js/tinymce/tiny_mce.js", "js/tinymce/textareas.js", "js/jquery-1.2.6.min.js", "js/adminimages.js")
 
 class FSAdmin(ArticleAdmin):
     fieldsets = (
