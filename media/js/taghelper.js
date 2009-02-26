9d4e8701272744bb4cc3acca83586c300f86749d
jnmbk <jnmbk@868bde64-912e-0410-afa2-bbf675bde9fd>
2009-02-26 16:25:13 +0000

    some comments for humanoids trying to read the code

--- media/js/taghelper.js
+++ media/js/taghelper.js
@@ -1,4 +1,5 @@
 function removeTag(tag, removeButton) {
+    /* deselects given tag (as string) from select box and removes it from tag list */
     $("#id_tags > option:selected").each(function (index, option){
         if (option.innerHTML == tag)
             return option.selected = false;
@@ -6,6 +7,7 @@ function removeTag(tag, removeButton) {
     $(removeButton).parent().fadeOut("fast", function(){ $(this).remove() });
 }
 function updateSelectedList() {
+    /* cleans and fills tag list by looking at selected items in selectbox */
     var selected = "";
     $("#id_tags > option:selected").each(function(index, option) {
         var tag = option.innerHTML;
@@ -15,13 +17,16 @@ function updateSelectedList() {
     $("#id_tags").before("<div id=\"selectedItems\">"+selected+"</div>");
 }
 $(document).ready(function() {
-    $("#tags").attr("id", "id_tags");
+    $("#tags").attr("id", "id_tags"); /* I'll use this on forum too, but it has tags instead of id_tags, that's why I'm modifying it */
     $("#id_tags").change(updateSelectedList);
+    /* create a tag input field for filtering through tags */
     $("#id_tags").before("<input id=\"tagfilter\" class=\"vTextField\" type=\"text\" />");
     updateSelectedList();
+    /* add autocompletion support to our tag input field, fills autocomplete list from the selectbox */
     $("#tagfilter").autocomplete({
         source:"#id_tags",
         onSelect:function(){
+            /* select the given tag in selectbox and update tag list */
             options = $("#id_tags > option");
             var i = 0;
             while (i<options.length && options[i].innerHTML!=$("#tagfilter").val()) {
