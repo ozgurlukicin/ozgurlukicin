d2d706f5f08e5100bc9e8219f40a37a112910507
jnmbk <jnmbk@868bde64-912e-0410-afa2-bbf675bde9fd>
2009-03-06 13:48:27 +0000

    even better looking tag helper

--- media/js/taghelper.js
+++ media/js/taghelper.js
@@ -1,3 +1,4 @@
+var smartTags = true;
 function removeTag(tag, removeButton) {
     /* deselects given tag (as string) from select box and removes it from tag list */
     $("#id_tags > option:selected").each(function (index, option){
@@ -14,13 +15,26 @@ function updateSelectedList() {
         selected = selected + "<div class=\"item\"><span class=\"tagName\">"+tag+"</span><span class=\"removeButton\" onclick=\"removeTag('"+tag+"', this)\">x</span></div>";
     });
     $("#selectedItems").remove();
-    $("#id_tags").before("<div id=\"selectedItems\">"+selected+"</div>");
+    $("#id_tags ~ .help:first").after("<div id=\"selectedItems\">"+selected+"</div>");
+}
+function toggleTagMode() {
+    if (smartTags) {
+        smartTags = false;
+        $("#id_tags, #add_id_tags, #id_tags ~ .help:last").show();
+        $("#tagfilter, #selectedItems, #id_tags ~ .help:first").hide();
+    } else {
+        smartTags = true;
+        $("#id_tags, #add_id_tags, #id_tags ~ .help:last").hide();
+        $("#tagfilter, #selectedItems, #id_tags ~ .help:first").show();
+    }
 }
 $(document).ready(function() {
     $("#tags").attr("id", "id_tags"); /* I'll use this on forum too, but it has tags instead of id_tags, that's why I'm modifying it */
     $("#id_tags").change(updateSelectedList);
     /* create a tag input field for filtering through tags */
-    $("#id_tags").before("<input id=\"tagfilter\" class=\"vTextField\" type=\"text\" />");
+    $("#id_tags, #add_id_tags, #id_tags ~ .help:last").hide();
+    $("#id_tags ~ .help:last").before("<input id=\"tagfilter\" class=\"vTextField\" type=\"text\" /><p class=\"help\">Bir etiket adı yazıp Enter'a basın.</p>");
+    $("#id_tags ~ .help:last").after("<a onclick=\"toggleTagMode();\" style=\"cursor:pointer\">[Tüm Etiketleri Göster/Gizle]</a>");
     updateSelectedList();
     /* add autocompletion support to our tag input field, fills autocomplete list from the selectbox */
     $("#tagfilter").autocomplete({
@@ -34,6 +48,7 @@ $(document).ready(function() {
             }
             options[i].selected = true;
             updateSelectedList();
+            $("#tagfilter").val("");
         },
         minchar:2
         });
