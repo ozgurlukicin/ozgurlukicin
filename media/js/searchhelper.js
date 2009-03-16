974393d65d45a38680ea349e333523da603845d4
jnmbk <jnmbk@868bde64-912e-0410-afa2-bbf675bde9fd>
2009-03-16 12:44:52 +0000

    better looking search with hide/show

---
+++ media/js/searchhelper.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $(".searchresults ul").toggle();
+    $(".searchresults .tags ul").show();
+    $(".searchresults > div").each(function (index, div){
+        $(div).find(".title").append("<img src=\"/media/img/new/button_toggle.png\" alt=\"[Gizle/Göster]\" />");
+        $(div).find(".title").click(function(){$("." + $(div).attr("class") + " ul").toggle();});
+    });
+});
