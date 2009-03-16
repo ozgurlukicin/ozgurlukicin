9f4dca6cad4be6a44c99eff1aed9df9062884d39
jnmbk <jnmbk@868bde64-912e-0410-afa2-bbf675bde9fd>
2009-03-16 13:34:05 +0000

    always open first result set

--- media/js/searchhelper.js
+++ media/js/searchhelper.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $(".searchresults ul").toggle();
-    $(".searchresults .tags ul").show();
+    $(".searchresults ul:first").show();
     $(".searchresults > div").each(function (index, div){
         $(div).find(".title").append("<img src=\"/media/img/new/button_toggle.png\" alt=\"[Gizle/Göster]\" />");
         $(div).find(".title").click(function(){$("." + $(div).attr("class") + " ul").toggle();});
