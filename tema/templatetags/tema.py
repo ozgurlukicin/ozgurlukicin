c706c3cb1c57b76354b79b0315593719cb6f7e1a
jnmbk <jnmbk@868bde64-912e-0410-afa2-bbf675bde9fd>
2009-08-04 11:53:00 +0000

    sidebar for top 10

--- tema/templatetags/tema.py
+++ tema/templatetags/tema.py
@@ -6,7 +6,8 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from django import template
-from oi.tema.models import ThemeAbuseReport
+from django.template import Context, loader
+from oi.tema.models import ThemeItem, ThemeAbuseReport
 
 register = template.Library()
 
@@ -14,3 +15,7 @@ register = template.Library()
 def number_of_theme_abuse():
     return ThemeAbuseReport.objects.count()
 
+@register.simple_tag
+def top_content():
+    object_list = ThemeItem.objects.filter(status=True).order_by("-rating", "-update")[:10]
+    return loader.get_template("tema/sidebar.html").render(Context({"object_list":object_list}))
