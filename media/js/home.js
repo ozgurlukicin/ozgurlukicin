7e9e57008254f3bbc84bc16e5ec48e89d26c1b50
jnmbk <jnmbk@868bde64-912e-0410-afa2-bbf675bde9fd>
2009-08-13 13:34:42 +0000

    working news selector

--- media/js/home.js
+++ media/js/home.js
@@ -1,16 +1,27 @@
 var intval=""
 $(function() {
-            intval = setInterval(function () {
-                change_news(1)
-            }, 10000)
-        });
-
+    intval = setInterval(function () {
+        change_news(1);
+    }, 10000)
+});
+var current_news = 0;
+var news_count = 5;
+$(document).ready(function() {
+    news_count = $(".news_container").length;
+});
+function select_news(i) {
+    $("#news_count>img").attr("src", "/media/img/new/news_progress.png");
+    $("#news_count>img:eq("+i+")").attr("src", "/media/img/new/news_progress_selected.png");
+};
 function change_news(direction) {
-    nc = $("#news_count").html();
-    $("#news_" + nc).hide();
-    if (nc == '1' && direction == -1) nc = 6;
-    if (nc == '5' && direction == 1) nc = 0;
-    nc = parseInt(nc) + direction;
-    $("#news_" + nc).fadeIn();
-    $("#news_count").html(nc);
+    $("#news_" + current_news).hide();
+    current_news = (current_news+direction) % news_count;
+    $("#news_" + current_news).fadeIn();
+    select_news(current_news);
+}
+function direct_select(i) {
+    $("#news_" + current_news).hide();
+    current_news = i;
+    $("#news_" + current_news).fadeIn();
+    select_news(current_news);
 }
