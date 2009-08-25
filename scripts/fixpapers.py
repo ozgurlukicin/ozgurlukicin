9522b7a406639a0046ada4f8a15bf9634a82fd91
jnmbk <jnmbk@868bde64-912e-0410-afa2-bbf675bde9fd>
2009-08-25 07:14:58 +0000

    script to make all wallpapers antialiased

---
+++ scripts/fixpapers.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# make all wallpapers antialiased
+
+import time
+
+import Image
+
+from oi.tema.models import Wallpaper
+
+for wallpaper in Wallpaper.objects.all():
+    if wallpaper.papers.count() > 1:
+        print "fixing", wallpaper
+        biggestWallpaper = wallpaper.papers.all()[0]
+        otherWallpapers = wallpaper.papers.all()[1:]
+
+        for smallWallpaper in otherWallpapers:
+            image = Image.open(biggestWallpaper.image.path)
+            if image.width == 1280 and image.height == 1024:
+                image = image.crop((0, 32, 1280, 992))
+
+            smallImage = Image.open(smallWallpaper.image.path)
+            image.thumbnail(smallImage.size, Image.ANTIALIAS)
+            image.save(smallWallpaper.image.path)
+            print "done", smallImage
+            time.sleep(0.1)
+    else:
+        print "skipping", wallpaper
