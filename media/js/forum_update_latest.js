e13dde00b4a0cded649737a6606227ac1a09b901
jnmbk <jnmbk@868bde64-912e-0410-afa2-bbf675bde9fd>
2009-09-29 09:35:13 +0000

    add ajax loader image and fix animation

--- media/js/forum_update_latest.js
+++ media/js/forum_update_latest.js
@@ -9,24 +9,27 @@ function syncData(data) {
     //insert new data
     $(data).each(function(i) {
         if ($(".latest").html().indexOf(this.id)<0) {
-            $(this).slideUp();
+            $(this).addClass("newcomer");
             $(".latest").prepend(this);
         }
     });
     //now do animations
-    $(".latest > .post").slideDown();
+    $(".latest > .newcomer").slideDown();
+    $(".latest > .newcomer").removeClass("newcomer");
     $(".tohide").slideUp("normal", function() {
         $(".tohide").remove();
     });
     }
+    $(".ajaxloader").hide();
 }
 var t;
 function updatePosts(delay) {
     var id = $(".latest > .post")[0].id;
+    $(".ajaxloader").show();
     $.get("/forum/son-iletiler/"+id+"/", syncData);
     t = setTimeout("updatePosts(" + delay + ")", delay);
 }
 $(document).ready(function() {
-    //get new posts every 20 seconds
-    updatePosts(20000);
+    //get new posts every 30 seconds
+    updatePosts(30000);
 });
