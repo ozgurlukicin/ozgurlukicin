e9d244a91c332dee6cb9e5e77d8c27ba6887d7dd
jnmbk <jnmbk@868bde64-912e-0410-afa2-bbf675bde9fd>
2010-01-27 14:41:05 +0000

    copy/paste/change

--- piyango/views.py
+++ piyango/views.py
@@ -5,14 +5,22 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
+from django.core.mail import EmailMessage
+from django.template import Context, loader
+
+from oi.forum.views import flood_control
+from oi.piyango.forms import PersonForm
+from oi.st.wrappers import render_response
+from oi.piyango.settings import PIYANGO_MAIL_LIST
+
 def create_person(request):
     if request.method == "POST":
-        form = CdClientForm(request.POST.copy())
+        form = PersonForm(request.POST.copy())
         flood, timeout = flood_control(request)
         if form.is_valid() and not flood:
-            cdClient = form.save()
-            message = loader.get_template("piyango/confirm_email.html").render(Context({"cdClient":cdClient}))
-            mail = EmailMessage("Pardus Çekilişi", message, "Özgürlükiçin <%s>" % DEFAULT_FROM_EMAIL, ["%s <%s>" % (cdClient.get_full_name(), cdClient.email)])
+            person = form.save()
+            message = loader.get_template("piyango/confirm_email.html").render(Context({"person":person}))
+            mail = EmailMessage("Pardus Çekilişi", message, "Özgürlükiçin <%s>" % DEFAULT_FROM_EMAIL, ["%s <%s>" % (person.get_full_name(), person.email)])
             mail.content_subtype = "html"
             mail.send(fail_silently=True)
             return render_response(request, "piyango/sent.html", locals())
@@ -23,37 +31,25 @@ def create_person(request):
             initial["last_name"] = request.user.last_name
             initial["email"] = request.user.email
             initial["city"] = request.user.get_profile().city
-        form = CdClientForm(initial=initial)
-    return render_response(request, "piyango/create_cdclient.html", locals())
+        form = PersonForm(initial=initial)
+    return render_response(request, "piyango/create_person.html", locals())
 
 def confirm_person(request, id, hash):
-    cdClient = get_object_or_404(CdClient, id=id, hash=hash, confirmed=False)
-    if CdClient.objects.filter(tcidentity=cdClient.tcidentity, confirmed=True).count()>0:
+    person = get_object_or_404(Person, id=id, hash=hash, confirmed=False)
+    if Person.objects.filter(tcidentity=person.tcidentity, confirmed=True).count()>0:
         return HttpResponse("Bu TC kimlik numarası daha önce kullanılmış!")
-    cdClient.confirmed = True
-    cdClient.save()
+    person.confirmed = True
+    person.save()
     #send mail to lists
-    message = loader.get_template("piyango/confirmed_email.html").render(Context({"cdClient":cdClient,"WEB_URL":WEB_URL}))
+    message = loader.get_template("piyango/confirmed_email.html").render(Context({"person":person,"WEB_URL":WEB_URL}))
     mail = EmailMessage(
-        "Pardus CD isteği",
+        "Pardus Çekilişi",
         message,
         "Özgürlükiçin <%s>" % DEFAULT_FROM_EMAIL,
-        [CD_MAIL_LIST],
-        headers={"Message-ID":"%s-%s" % (cdClient.id, cdClient.hash)}
+        [PIYANGO_MAIL_LIST],
+        headers={"Message-ID":"%s-%s" % (person.id, person.hash)}
     )
     mail.content_subtype = "html"
     mail.send(fail_silently=True)
 
     return render_response(request, "piyango/confirmed.html", locals())
-
-@permission_required("piyango.change_person")
-def cdclient_list(request):
-    if request.method == "POST":
-        form = SearchForm(request.POST.copy())
-        if form.is_valid():
-            term = form.cleaned_data["term"]
-            cdClient_list = CdClient.objects.filter(Q(first_name__icontains=term)|Q(last_name__icontains=term)|Q(address__icontains=term)|Q(phone_number__icontains=term)).filter(sent=False, confirmed=True)
-    else:
-        form = SearchForm()
-
-    return render_response(request, "piyango/person_list.html", locals())
