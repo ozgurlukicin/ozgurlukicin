4c95bf8b4b0f2cd2477c6236e7145763bede276d
gkmngrgn <gkmngrgn@868bde64-912e-0410-afa2-bbf675bde9fd>
2010-04-05 13:32:16 +0000

    force utf-8

--- scripts/kargoxml.py
+++ scripts/kargoxml.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import locale
 from oi.shipit.models import CdClient
 from xml.etree import ElementTree as ET
 
 
 def add_column(cargo):
+    locale.setlocale(locale.LC_ALL, "tr_TR.UTF-8")
     cdclient = CdClient.objects.filter(confirmed=1, sent=0, taken=0)[:300]
     root = ET.Element('document')
 
