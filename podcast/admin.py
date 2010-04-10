77859ec48267d6413148734058dcc9c2b9ad674c
gkmngrgn <gkmngrgn@868bde64-912e-0410-afa2-bbf675bde9fd>
2010-04-10 12:40:22 +0000

    disabled guests

--- podcast/admin.py
+++ podcast/admin.py
@@ -10,7 +10,7 @@ from django.contrib import admin
 from oi.podcast.models import *
 
 class EpisodeAdmin(admin.ModelAdmin):
-    exclude = ("topic", 'guests')
+    exclude = ("topic")
     class Media:
         js = ("js/tinymce/tiny_mce.js", "js/tinymce/textareas.js", "js/jquery-1.2.6.min.js", "js/adminimages.js", "js/jquery.autocomplete.js", "js/taghelper.js")
         css = {
