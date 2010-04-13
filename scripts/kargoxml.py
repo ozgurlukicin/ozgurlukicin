4ca74810e4eb3c2dec88d8962f428fa7a7adb7cf
gkmngrgn <gkmngrgn@868bde64-912e-0410-afa2-bbf675bde9fd>
2010-04-13 15:06:06 +0000

    simple is the best.

--- scripts/kargoxml.py
+++ scripts/kargoxml.py
@@ -23,18 +23,25 @@ def add_column(limit):
 
     for field in cdclient:
         cargo = ET.SubElement(root, 'cargo')
+        sub_elements = {
+            'receiver_name': '%s %s' % (field.first_name, field.last_name),
+            'receiver_address': field.address,
+            'city': field.city,
+            'town': field.town,
+            'email_address': field.email,
+            'tax_number': field.tcidentity,
+            'cargo_count': "1",
+            'cargo_type': "0",
+            'payment_type': "0",
+            'dispatch_number': " ",
+            'referans_number': " ",
+            'cargo_content': "Pardus Kurulum CD'si",
+            'collection_type': "0",
+            'invoice_number': " ",
+            'invoice_amount': "2,36"}
 
-        receiver_name = ET.SubElement(cargo, 'receiver_name')
-        receiver_name.text = '%s %s' % (field.first_name, field.last_name)
-
-        receiver_address = ET.SubElement(cargo, 'receiver_address')
-        receiver_address.text = field.address
-
-        city = ET.SubElement(cargo, 'city')
-        city.text = field.city
-
-        town = ET.SubElement(cargo, 'town')
-        town.text = field.town
+        for element in sub_elements:
+            ET.SubElement(cargo, element).text = sub_elements[element]
 
         phone_work = ET.SubElement(cargo, 'phone_work')
         phone_gsm = ET.SubElement(cargo, 'phone_gsm')
@@ -45,39 +52,6 @@ def add_column(limit):
         else:
             phone_work.text = '%s%s' % (field.phone_area, field.phone_number)
             phone_gsm.text = ' '
-
-        email_address = ET.SubElement(cargo, 'email_address')
-        email_address.text = field.email
-
-        tax_number = ET.SubElement(cargo, 'tax_number')
-        tax_number.text = field.tcidentity
-
-        cargo_count = ET.SubElement(cargo, 'cargo_count')
-        cargo_count.text = "1"
-
-        cargo_type = ET.SubElement(cargo, 'cargo_type')
-        cargo_type.text = "0"
-
-        payment_type = ET.SubElement(cargo, 'payment_type')
-        payment_type.text = "0"
-
-        dispatch_number = ET.SubElement(cargo, 'dispatch_number')
-        dispatch_number.text = ' '
-
-        referans_number = ET.SubElement(cargo, 'referans_number')
-        referans_number.text = ' '
-
-        cargo_content = ET.SubElement(cargo, 'cargo_content')
-        cargo_content.text = "Pardus Kurulum CD'si"
-
-        collection_type = ET.SubElement(cargo, 'collection_type')
-        collection_type.text = '0'
-
-        invoice_number = ET.SubElement(cargo, 'invoice_number')
-        invoice_number.text = ' '
-
-        invoice_amount = ET.SubElement(cargo, 'invoice_amount')
-        invoice_amount.text = '2,36'
 
     tree = ET.ElementTree(root)
     tree.write('kargo.xml', encoding='utf-8')
