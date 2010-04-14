70da36957192c10bf279e5e8cf31cfff690302a4
gkmngrgn <gkmngrgn@868bde64-912e-0410-afa2-bbf675bde9fd>
2010-04-14 10:13:35 +0000

    optimization

--- scripts/kargo/kargoxml.py
+++ scripts/kargo/kargoxml.py
@@ -1,22 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import sys
-import os
-
-script_dir = os.path.split(os.path.abspath(os.path.dirname(__file__)))[0]
-project_dir = os.path.split(script_dir)[0]
-sys.path.append(project_dir)
-sys.path.append(os.path.split(project_dir)[0])
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-
-from oi.shipit.models import CdClient
 from xml.etree import ElementTree as ET
 
 
-def add_column(limit, timestamp):
-    cdclient = CdClient.objects.filter(confirmed=1,
-        sent=0, taken=0).order_by('date')[:limit]
+def add_column(cdclient, timestamp):
     root = ET.Element('document')
 
     for field in cdclient:
--- scripts/kargo/main.py
+++ scripts/kargo/main.py
@@ -2,18 +2,35 @@
 # -*- coding: utf-8 -*-
 
 import locale
+import os
 import sys
 from datetime import date
 
 from kargoxml import add_column
 
+script_dir = os.path.split(os.path.abspath(os.path.dirname(__file__)))[0]
+project_dir = os.path.split(script_dir)[0]
+sys.path.append(project_dir)
+sys.path.append(os.path.split(project_dir)[0])
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+from oi.shipit.models import CdClient
+
 
 if __name__ == '__main__':
-    locale.setlocale(locale.LC_ALL, "tr_TR.UTF-8")
     args = sys.argv
-
     if len(args) != 2:
         print("Usage: python %s [limit]") % __file__
         sys.exit()
 
-    add_column(args[-1], date.today().isoformat())
+    try:
+        limit = int(args[-1])
+    except ValueError:
+        print("Invalid limit: %s") % args[-1]
+        sys.exit()
+
+    locale.setlocale(locale.LC_ALL, "tr_TR.UTF-8")
+    cdclient = CdClient.objects.filter(confirmed=1,
+        sent=0, taken=0).order_by('date')[:limit]
+
+    add_column(cdclient, date.today().isoformat())
