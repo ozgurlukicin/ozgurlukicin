62d6d8222861d1acfd927b234b055ea0f6e50d75
jnmbk <jnmbk@868bde64-912e-0410-afa2-bbf675bde9fd>
2010-06-15 13:13:16 +0000

    begin translating py files

--- ezine/models.py
+++ ezine/models.py
@@ -2,23 +2,24 @@
 
 from django.db import models
 from oi.upload.models import Image
+from django.utils.translation import ugettext as _
 
 class Ezine(models.Model):
-    issue = models.PositiveIntegerField("Sayı", primary_key=True, unique=True)
-    title = models.CharField('Başlık', max_length=64, help_text="alt satıra düşmesi için arada &lt;br/&gt; kullanın \"Özgürlükİçin e-dergi &lt;br/&gt; Şubat / Mart 2009\" gibi")
-    description = models.TextField('İçindekiler')
-    image = models.ForeignKey(Image, verbose_name="Dergi Görseli", blank=True, null=True, help_text="Görselin 310x205 boyutlarında olmasına dikkat edin! Yeni görsel eklemek için + düğmesine tıklayın.")
-    editor = models.CharField('Editör', max_length=64)
-    page_count = models.PositiveIntegerField("Sayfa sayısı")
-    download_300dpi = models.CharField('300dpi dergi adresi', max_length=256, help_text="/media/dosya/oi_say11_buyuk.pdf gibi")
-    size_300dpi = models.PositiveIntegerField("300dpi dergi boyutu", help_text="bayt cinsinden")
-    download_96dpi = models.CharField('96dpi dergi adresi', max_length=256, help_text="/media/dosya/oi_say11_kucuk.pdf gibi")
-    size_96dpi = models.PositiveIntegerField("96dpi dergi boyutu", help_text="bayt cinsinden")
+    issue = models.PositiveIntegerField(_("Issue"), primary_key=True, unique=True)
+    title = models.CharField(_("Title"), max_length=64, help_text=_("use &lt;br/&gt; to break line like \"Spread Pardus E-zine &lt;br/&gt; March 2009\""))
+    description = models.TextField(_("Contents"))
+    image = models.ForeignKey(Image, verbose_name=_("E-zine Image"), blank=True, null=True, help_text="Note that image should be 310x205 in size! Press + to add new image.")
+    editor = models.CharField(_('Editor'), max_length=64)
+    page_count = models.PositiveIntegerField(_("Page count"))
+    download_300dpi = models.CharField(_("300dpi e-zine address"), max_length=256, help_text=_("like /media/dosya/sp_i11_big.pdf"))
+    size_300dpi = models.PositiveIntegerField(_("300dpi e-zine size"), help_text=_("in bytes"))
+    download_96dpi = models.CharField(_("96dpi e-zine address"), max_length=256, help_text=_("like /media/dosya/sp_i11_sml.pdf"))
+    size_96dpi = models.PositiveIntegerField(_("96dpi e-zine size"), help_text=_("in bytes"))
     is_active = models.BooleanField()
 
     def __unicode__(self):
         return self.title
 
     class Meta:
-        verbose_name = "E-Dergi"
-        verbose_name_plural = "E-Dergiler"
+        verbose_name = _("E-Zine")
+        verbose_name_plural = _("E-Zines")
