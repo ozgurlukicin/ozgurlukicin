8f8d43c31133cfe2b16b4cbd0f8d57316700158f
jnmbk <jnmbk@868bde64-912e-0410-afa2-bbf675bde9fd>
2010-06-18 11:44:59 +0000

    translated seminar application

--- seminar/models.py
+++ seminar/models.py
@@ -8,40 +8,42 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.utils.translation import ugettext as _
+
 class Place(models.Model):
-    name = models.CharField('Seminer Yeri', max_length=64, blank=False, unique=True)
-    description = models.TextField("Yer Tanımı", max_length=512, blank=True)
-    phone = models.CharField("Telefon", max_length=16, blank=True)
-    latitude = models.DecimalField('Enlem', max_digits=10, decimal_places=6, default=0, help_text="40.903823 gibi")
-    longitude = models.DecimalField('Boylam', max_digits=10, decimal_places=6, default=0, help_text="29.226723 gibi")
+    name = models.CharField(_("Seminar Location"), max_length=64, blank=False, unique=True)
+    description = models.TextField(_("Location Description"), max_length=512, blank=True)
+    phone = models.CharField(_("Telephone"), max_length=16, blank=True)
+    latitude = models.DecimalField(_("Latitude"), max_digits=10, decimal_places=6, default=0, help_text=_("like 40.903823"))
+    longitude = models.DecimalField(_("Longitude"), max_digits=10, decimal_places=6, default=0, help_text=_("like 29.226723"))
 
     def __unicode__(self):
         return self.name
 
     class Meta:
         ordering = ['name']
-        verbose_name = "Seminer Yeri"
-        verbose_name_plural = "Seminer Yerleri"
+        verbose_name = _("Seminar Location")
+        verbose_name_plural = _("Seminar Locations")
 
 class Attender(models.Model):
-    name = models.CharField('Gerçek Adı', max_length=64, blank=False, unique=True)
-    user = models.ForeignKey(User, verbose_name="Öİ Kullanıcı Adı", blank=True, null=True, help_text="Zorunlu değil")
+    name = models.CharField(_("Real Name"), max_length=64, blank=False, unique=True)
+    user = models.ForeignKey(User, verbose_name=_("Username"), blank=True, null=True, help_text=_("Not required"))
 
     def __unicode__(self):
         return self.name
 
     class Meta:
         ordering = ['name']
-        verbose_name = "Konuşmacı"
-        verbose_name_plural = "Konuşmacılar"
+        verbose_name = _("Speaker")
+        verbose_name_plural = _("Speakers")
 
 class Seminar(models.Model):
-    description = models.CharField('Seminer tanımı', max_length = 64, blank = False)
-    place = models.ForeignKey(Place, verbose_name="Yer")
-    attender = models.ManyToManyField(Attender, verbose_name="Katılımcılar")
-    start_date = models.DateTimeField('Başlangıç', blank=False)
-    end_date = models.DateTimeField('Bitiş', blank=True, null=True, help_text="zorunlu değil")
-    status = models.BooleanField('Aktif')
+    description = models.CharField(_("Seminar description"), max_length = 64, blank = False)
+    place = models.ForeignKey(Place, verbose_name=_("Location"))
+    attender = models.ManyToManyField(Attender, verbose_name=_("Attenders"))
+    start_date = models.DateTimeField(_("Start date"), blank=False)
+    end_date = models.DateTimeField(_("End date"), blank=True, null=True, help_text=_("Not required"))
+    status = models.BooleanField(_("Active"))
 
     def __unicode__(self):
         return self.description
@@ -50,5 +52,5 @@ class Seminar(models.Model):
         return "/seminer/%d/" % self.id
 
     class Meta:
-        verbose_name = "Seminer"
-        verbose_name_plural = "Seminerler"
+        verbose_name = _("Seminar")
+        verbose_name_plural = _("Seminars")
