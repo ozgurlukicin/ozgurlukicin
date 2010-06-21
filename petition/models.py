935746ea341ff3b3724d9483ba55537e2840fe01
jnmbk <jnmbk@868bde64-912e-0410-afa2-bbf675bde9fd>
2010-06-21 14:02:34 +0000

    reverse translation

--- petition/models.py
+++ petition/models.py
@@ -11,24 +11,26 @@ from django.contrib import admin
 
 from oi.settings import CITY_LIST
 
+from django.utils.translation import ugettext as _
+
 class Petitioner(models.Model):
-    firstname = models.CharField("Ad", max_length=30)
-    lastname = models.CharField("Soyad", max_length=30)
-    city = models.CharField("Şehir", blank=True, choices=CITY_LIST, max_length=40)
-    job = models.CharField("Meslek", max_length=30)
-    email = models.EmailField("E-posta", max_length=50)
-    homepage = models.URLField("Ana Sayfa", blank=True, verify_exists=False, unique=False)
-    signed = models.DateTimeField(blank=True, null=True, auto_now_add=True, verbose_name='İmzalama tarihi')
-    activation_key = models.CharField("Etkinleştirme Anahtarı", max_length=40)
-    is_active = models.BooleanField("Etkin")
-    inform = models.BooleanField("Haberdar Et")
+    firstname = models.CharField(_("Name"), max_length=30)
+    lastname = models.CharField(_("Surname"), max_length=30)
+    city = models.CharField(_("City"), blank=True, choices=CITY_LIST, max_length=40)
+    job = models.CharField(_("Job"), max_length=30)
+    email = models.EmailField(_("E-mail"), max_length=50)
+    homepage = models.URLField(_("Mainpage"), blank=True, verify_exists=False, unique=False)
+    signed = models.DateTimeField(blank=True, null=True, auto_now_add=True, verbose_name=_("Sign date"))
+    activation_key = models.CharField(_("Activation Key"), max_length=40)
+    is_active = models.BooleanField(_("Active"))
+    inform = models.BooleanField(_("Inform"))
 
     def __unicode__(self):
         return "%s %s" % (self.firstname, self.lastname)
 
     class Meta:
-        verbose_name = "İmzalayan"
-        verbose_name_plural = "İmzalayanlar"
+        verbose_name = _("Signer")
+        verbose_name_plural = _("Signers")
 
 class PetitionerAdmin(admin.ModelAdmin):
     list_display = ("firstname", "lastname", "job", "city", "email", "homepage", "signed", "is_active", "inform")
@@ -38,13 +40,13 @@ class PetitionerAdmin(admin.ModelAdmin):
 admin.site.register(Petitioner, PetitionerAdmin)
 
 class PetitionForm(forms.Form):
-    firstname = forms.CharField(label="Ad:", max_length=30)
-    lastname = forms.CharField(label="Soyad:", max_length=30)
-    city = forms.ChoiceField(label="Şehir:", choices=CITY_LIST)
-    job = forms.CharField(label="Meslek:", max_length=30)
-    email = forms.EmailField(label="E-Posta:")
-    homepage = forms.URLField(label="Web Sayfası:", verify_exists=False, required=False, help_text="(zorunlu değil)")
-    inform = forms.BooleanField(label="Etkinliklerden haberdar et:", required=False, help_text="Özgürlükİçin'in diğer etkinliklerinden haberdar olmak için işaretleyin.")
+    firstname = forms.CharField(label=_("Name"), max_length=30)
+    lastname = forms.CharField(label=_("Surname"), max_length=30)
+    city = forms.ChoiceField(label=_("City"), choices=CITY_LIST)
+    job = forms.CharField(label=_("Job"), max_length=30)
+    email = forms.EmailField(label=_("E-Mail"))
+    homepage = forms.URLField(label=_("Web Site"), verify_exists=False, required=False, help_text=_("(not required)"))
+    inform = forms.BooleanField(label=_("Inform me about events:"), required=False, help_text=_("Select this if you want to get news about other Spread Pardus events."))
 
     def clean_email(self):
         field_data = self.cleaned_data["email"]
@@ -54,6 +56,6 @@ class PetitionForm(forms.Form):
 
         u = Petitioner.objects.filter(email=field_data)
         if len(u) > 0:
-            raise forms.ValidationError(u"Bu e-posta adresi ile daha önceden kayıt yapılmış")
+            raise forms.ValidationError(_("This e-mail address has already signed the petition."))
 
         return field_data
