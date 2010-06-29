ca6a55cd673f9ca6c120fcb2f9620ed9546b9096
ufuk.uzun <ufuk.uzun@868bde64-912e-0410-afa2-bbf675bde9fd>
2010-06-29 14:53:17 +0000

    forms.py created

---
+++ beyin2/forms.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TÜBİTAK UEKAE
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from django import forms
+from oi.beyin2.models import Idea
+
+class IdeaForm(forms.ModelForm):
+    class Meta:
+	model = Idea
+	exclude = ('dateSubmitted', 'submitter')
