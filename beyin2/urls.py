120ed807037bc0ca51b1963852c5a970ff9578a8
engin.manap <engin.manap@868bde64-912e-0410-afa2-bbf675bde9fd>
2010-06-30 11:53:33 +0000

    there was 1 too many slash, removed

--- beyin2/urls.py
+++ beyin2/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('oi.beyin2.views',
 
     (r'^$', 'main'),
     url(r'^add/$','add_new', name='add_new'),
-    url(r'^/idea_(?P<idea_id>\d+)/edit/$','edit_idea', name='edit_idea'),
+    url(r'^idea_(?P<idea_id>\d+)/edit/$','edit_idea', name='edit_idea'),
     #url(r'^(?P<blog_id>\d+)/$',"blog_goster",name="blog_goster"),
     #url(r'^(?P<blog_id>\d+)/(?P<yazi_id>\d+)/yorum_ekle/$',"yorum_ekle",name="yorum_ekle"),
 )
