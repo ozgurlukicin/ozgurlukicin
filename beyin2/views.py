63454ed7ef9b1b63167966c1f3b177f3abe0fafa
ufuk.uzun <ufuk.uzun@868bde64-912e-0410-afa2-bbf675bde9fd>
2010-06-30 11:39:59 +0000

    views.py changed, idea_new_error.html added

--- beyin2/views.py
+++ beyin2/views.py
@@ -5,17 +5,38 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
-from django.http import HttpResponse
-from oi.beyin2.models import Idea
+from django.http import HttpResponse, HttpResponseRedirect
+from oi.beyin2.models import Idea, Status, Category
 from oi.beyin2.forms import IdeaForm
 from oi.st.wrappers import render_response
+from django.contrib.auth.decorators import login_required, permission_required
+from django.core.urlresolvers import reverse
+
+DefaultCategory = 4
+DefaultStatus = 3
 
 def main(request):
     idea_list = Idea.objects.all().order_by('-dateSubmitted')[:10]
     return render_response(request,'beyin2/idea_list.html',{'idea_list': idea_list})
 
+@login_required
 def add_new(request):
-    form = IdeaForm({'title': '', 'description': '', 'status': '', 'category': ''})
+    try:
+	form = IdeaForm({'title': '', 'description': '', 'status': DefaultStatus, 'category': DefaultCategory})
+	if request.POST:	
+	    form = IdeaForm(request.POST)
+	    if form.is_valid():
+		i = form.save(commit = False)
+		i.submitter = request.user
+		i.save()
+		return HttpResponseRedirect(reverse('oi.beyin2.models.main'))
+	else:
+	    return render_response(request, 'beyin2/idea_new.html', {'form':form})
+    except:
+	return render_response(request, 'beyin2/idea_new_errorpage.html')
 
-    return render_response(request,'beyin2/idea_new.html',{'form':form,})
+@permission_required('beyin2.change_idea')
+def edit_idea(request, idea_id):
+    return HttpResponse("Edit sayfası")
 
+    
