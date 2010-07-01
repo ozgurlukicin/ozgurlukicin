d45704722f62b6ffd8e8b197a74f854fce0cef5b
gkmngrgn <gkmngrgn@868bde64-912e-0410-afa2-bbf675bde9fd>
2010-07-01 13:35:05 +0000

    update script for new cdclient model.

--- scripts/kargo/kargoxml.py
+++ scripts/kargo/kargoxml.py
@@ -24,20 +24,12 @@ def add_column(cdclient, timestamp):
             'cargo_content': "Pardus Kurulum CD'si",
             'collection_type': "0",
             'invoice_number': " ",
-            'invoice_amount': "2,36"}
+            'invoice_amount': "2,36",
+            'phone_work': '%s%s' % (field.phone_area, field.phone_number),
+            'phone_gsm': '%s%s' % (field.gsm_area, field.gsm_number)}
 
         for element in sub_elements:
             ET.SubElement(cargo, element).text = sub_elements[element]
-
-        phone_work = ET.SubElement(cargo, 'phone_work')
-        phone_gsm = ET.SubElement(cargo, 'phone_gsm')
-
-        if field.phone_area.startswith('5'):
-            phone_work.text = ' '
-            phone_gsm.text = '%s%s' % (field.phone_area, field.phone_number)
-        else:
-            phone_work.text = '%s%s' % (field.phone_area, field.phone_number)
-            phone_gsm.text = ' '
 
     tree = ET.ElementTree(root)
     tree.write('kargo_%s.xml' % timestamp, encoding='utf-8')
