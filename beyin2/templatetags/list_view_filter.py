4bdb51bd65fba25ccaffd4525cb377c8e92cc833
engin.manap <engin.manap@868bde64-912e-0410-afa2-bbf675bde9fd>
2010-07-08 21:23:48 +0000

    upgraded to working versions

--- beyin2/templatetags/list_view_filter.py
+++ beyin2/templatetags/list_view_filter.py
@@ -1,12 +1,15 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
 from django import template
-from oi.beyin2.models import Vote, Idea
+from django.core.urlresolvers import reverse
+
+
 
 register = template.Library()
-
-
 @register.inclusion_tag('beyin2/tt_idea_vote.html')
-def is_voted(idea,user):
-    vote = idea.objects.filter(voter=user)
-    return {'vote':vote[0].vote,'idea':idea,'idea':idea}
+def is_voted(idea, user):
+    vote = idea.vote_set.filter( voter=user )
+    if vote:
+        return {'idea':idea,'vote':vote[0].vote}
+        #if not exist send Y
+    else:
+        return {'idea':idea,'vote': "Y"}
+
