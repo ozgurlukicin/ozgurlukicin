df51de40770772ce2306f8b2c103043ccff5dc97
gkmngrgn <gkmngrgn@868bde64-912e-0410-afa2-bbf675bde9fd>
2010-11-25 13:01:12 +0000

    not required to render_response

--- flatpages/views.py
+++ flatpages/views.py
@@ -1,5 +1,4 @@
 from oi.flatpages.models import FlatPage
-from oi.st.wrappers import render_response
 from django.template import loader, RequestContext
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
