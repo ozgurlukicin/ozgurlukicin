a73804be98daf08b7f97c308984dda62af62b2ca
ilker <ilker@868bde64-912e-0410-afa2-bbf675bde9fd>
2010-12-11 18:44:19 +0000

    order by rating list also exists on the top menu, remove it from sidebar

--- tema/templatetags/tema.py
+++ tema/templatetags/tema.py
@@ -7,27 +7,10 @@
 
 from django import template
 from django.template import Context, loader
-from oi.tema.models import ThemeItem, ThemeAbuseReport, DesktopScreenshot, Wallpaper, Font
+from oi.tema.models import ThemeAbuseReport
 
 register = template.Library()
 
 @register.simple_tag
 def number_of_theme_abuse():
     return ThemeAbuseReport.objects.count()
-
-@register.simple_tag
-def top_content(category=None):
-    if category == "duvar-kagitlari":
-        object_list = Wallpaper.objects.all()
-        title = "En Beğenilen Duvar Kağıtları"
-    elif category == "masaustu-goruntuleri":
-        object_list = DesktopScreenshot.objects.all()
-        title = "En Beğenilen Masaüstleri"
-    elif category == "yazitipleri":
-        object_list = Font.objects.all()
-        title = "En Beğenilen Yazıtipleri"
-    else:
-        object_list = ThemeItem.objects.all()
-        title = "En Çok Beğenilenler"
-    object_list = object_list.filter(status=True).order_by("-rating", "-update")[:10]
-    return loader.get_template("tema/sidebar.html").render(Context({"object_list":object_list,"title":title}))
