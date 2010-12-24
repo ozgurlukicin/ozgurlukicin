b3d599a7a0bd0f761bac19dea132818cc4a410d5
ilker <ilker@868bde64-912e-0410-afa2-bbf675bde9fd>
2010-12-24 15:52:59 +0000

    fix for #83

--- media/js/adminimages.js
+++ media/js/adminimages.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     var image = $("#id_image");
     image.attr("onchange", "update_image(this)");
     image.attr("onkeyup", "update_image(this)");
-    $(".image").before("<div class=\"form-row image_filter\"><div><label for=\"image_filter\">Görselleri Filtrele</label><input type=\"text\" maxlength=\"32\" name=\"image_filter\" class=\"vTextField\" id=\"filter\"/><p class=\"help\">Burayı kullanarak görsellerde arama yapabilirsiniz.</p></div></div>");
+    $(".image").before("<div class=\"row cells-1 image_filter\"><div><div class=\"column span-4\"><label for=\"image_filter\">Görselleri Filtrele:</label></div><div class=\"column span-flexible\"><input type=\"text\" maxlength=\"32\" name=\"image_filter\" class=\"vTextField\" id=\"filter\"/><p class=\"help\">Burayı kullanarak görsellerde arama yapabilirsiniz.</p></div></div></div>");
     $(".image > div").after("<img src=\"\" id=\"image_thumb\" alt=\"Üstteki arama kutusunu kullanıp listeden bir görsel seçiniz\" />");
     $("#filter").attr("onchange", "filter_images()");
     $("#filter").attr("onkeyup", "filter_images()");
