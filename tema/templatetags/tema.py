d1240fc824dd99ad4685d6e77998121f2d208455
ilker <ilker@868bde64-912e-0410-afa2-bbf675bde9fd>
2010-12-25 13:47:50 +0000

    fresh_or_updated function added

--- tema/templatetags/tema.py
+++ tema/templatetags/tema.py
@@ -8,9 +8,22 @@
 from django import template
 from django.template import Context, loader
 from oi.tema.models import ThemeAbuseReport
+import datetime
 
 register = template.Library()
 
 @register.simple_tag
 def number_of_theme_abuse():
     return ThemeAbuseReport.objects.count()
+
+@register.simple_tag
+def fresh_or_updated(item):
+    delta = datetime.datetime.now() - item.update
+    if delta.days < 11:
+        if item.submit == item.update:
+            result = "fresh"
+        else:
+            result = "updated"
+
+        return "<img src=\"/media/img/tema/new/%s.png\" />" % result
+    return ""
