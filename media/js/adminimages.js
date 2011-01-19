59a66abb51b6417a6b4a546feed236e708b3e123
ilker <ilker@868bde64-912e-0410-afa2-bbf675bde9fd>
2011-01-19 13:12:28 +0000

    fix for #84

--- media/js/adminimages.js
+++ media/js/adminimages.js
@@ -1,15 +1,13 @@
-function update_image(selectBox) {
-    $("#image_thumb").attr("src", "/media/" + selectBox[selectBox.selectedIndex].innerHTML);
+function update_image() {
+    var path = $("#id_image option:selected").text()
+    $("#image_thumb").attr("src", "/media/" + path);
 }
 $(document).ready(function() {
-    var image = $("#id_image");
-    image.attr("onchange", "update_image(this)");
-    image.attr("onkeyup", "update_image(this)");
+    $("#id_image").bind("change", update_image);
     $(".image").before("<div class=\"row cells-1 image_filter\"><div><div class=\"column span-4\"><label for=\"image_filter\">Görselleri Filtrele:</label></div><div class=\"column span-flexible\"><input type=\"text\" maxlength=\"32\" name=\"image_filter\" class=\"vTextField\" id=\"filter\"/><p class=\"help\">Burayı kullanarak görsellerde arama yapabilirsiniz.</p></div></div></div>");
     $(".image > div").after("<img src=\"\" id=\"image_thumb\" alt=\"Üstteki arama kutusunu kullanıp listeden bir görsel seçiniz\" />");
-    $("#filter").attr("onchange", "filter_images()");
-    $("#filter").attr("onkeyup", "filter_images()");
-    update_image(document.getElementById("id_image"));
+    $("#filter").bind("change keyup", filter_images);
+    update_image();
 });
 function filter_images() {
     if ($("#filter").val().length > 2) {
